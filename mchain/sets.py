730bb48dcbd25c618d4993657d9b5ac0bbe61f92
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-12-11 16:20:47 +0000

    set param

---
+++ mchain/sets.py
@@ -0,0 +1,32 @@
+import itertools
+from pprint import pprint
+import chain_lib
+
+
+class SetPoint(object):
+	"""Point which is a set param"""
+	def __init__(self, num_elems):
+		super(SetPoint, self).__init__()
+		self.num_elems = num_elems
+		self.size = 2 ** self.num_elems - 1
+
+	def random_point(self):
+		u = chain_lib.uniform_int(0, self.size)
+		# print(bin(u))
+		# print(u)
+		# print(self.as_list(u))
+		# return (self.as_list(u))
+		return u
+
+	def as_list(self, bits):
+		return [ i for i in range(self.num_elems) if bits >> i & 1 == 1]
+
+
+def test(size, times):
+	s = SetPoint(size)
+	arr = []
+	for k, g in itertools.groupby(sorted([ s.as_list(s.random_point()) for i in range(times) ])):
+		arr.append( (len(list(g)), k) )
+
+	pprint(sorted(arr))
+	print(len(arr))
