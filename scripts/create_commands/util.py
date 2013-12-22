ac05841eaa4f7692926c502146b9565d8e98cbf7
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-12-22 06:43:50 +0000

    Make sure that the number of races happen

--- scripts/create_commands/util.py
+++ scripts/create_commands/util.py
@@ -2,7 +2,7 @@ import math
 
 
 def calc_models_timeout(common, cores):
-	return math.ceil(math.ceil(common['total_time'] / common['races']) / cores)
+	return math.ceil(common['total_time'] / (common['races'] + 1) / cores)
 
 
 def calc_total_time(common, cores):
