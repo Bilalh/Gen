5d3ee1437ae778229c1137232805d5021d827903
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-12-22 05:19:59 +0000

    Update meta-script to do more stuff in parallel

--- scripts/create_commands/markov.py
+++ scripts/create_commands/markov.py
@@ -1,6 +1,7 @@
 import math
 import os
 import util
+from pprint import pprint
 
 
 def create_commands(data, commons_grouped, place_dir, init_source, num_runs):
@@ -20,38 +21,62 @@ def create_commands(data, commons_grouped, place_dir, init_source, num_runs):
 			"### markov ###",
 			init_source,
 			"#-- {} --#".format(filepath),
-			"for race_no in {1..%d}; do" % (num_runs)
 		]
 
-		for common in commons:
-			tu = (int(math.ceil(common['total_time'] / 60 / 60)), common['races'] )
-			lines.append("		# {:03}h, {:03} races".format(*tu) )
+		par_function = """
+Command=$( cat <<EOF
+record_cp {log_path}__{race_no}/logs/log-{race_no} ../instancegen/mchain/chain_sampling.py time {limit} \\
+		--models_timeout={models_timeout} \\
+		--mode=%s \\
+		--select_radius={select_radius} --influence_radius={influence_radius} \\
+		--chain_length={chain_length} \\
+		--essence=%s --working_dir=%s --output_dir={output_dir}__{race_no} %s
 
-			extra = "out-{:03}-{:03}__{race_no}".format(*tu, race_no="${race_no}")
+EOF
+)""" % (
+			mode,
+			filepath,
+			os.path.dirname(filepath),
+			" ".join([ " --{}".format(k) for (k, v) in cur.items() if v is True ])
+		)
+
+		line = 'parallel  --header : --dry-run --tagstring "run-{1}" -j%d $Command ' % (cores / num_models)
+		line += '\\\n ::: race_no `seq 1 %d` \\' % (num_runs)
+
+
+		def build_dict(common):
+			tu = (int(math.ceil(common['total_time'] / 60 / 60)), common['races'] )
+
+			extra = "out-{:03}-{:03}".format(*tu)
 			settings = {
-				"essence": filepath,
-				"essence_dir": os.path.dirname(filepath),
 				"models_timeout": util.calc_models_timeout(common, jobs),
 				"limit": util.calc_total_time(common, jobs),
-				"mode": mode,
 				"output_dir": os.path.join(place_dir, "results", "markov", name, extra),
-				"log_path": os.path.join(place_dir, "results", "markov", name, extra, "logs", "log-${race_no}")
+				"log_path": os.path.join(place_dir, "results", "markov", name, extra)
 			}
+			lines.append("# {:03}h {limit}s ".format(*tu, **settings))
+
+
 			settings.update(cur)
-			# print(settings)
-			command ="\t" + """
-			record_cp {log_path} ../instancegen/mchain/chain_main.py time {limit}\
-				--models_timeout={models_timeout}\
-				--mode={mode}\
-				--select_radius={select_radius} --influence_radius={influence_radius} \
-				--chain_length={chain_length} \
-				--essence={essence} --working_dir={essence_dir} --output_dir={output_dir}
-			""".format(**settings).strip().replace("\t", " ")
-			command += " ".join([ " --{}".format(k) for (k, v) in cur.items() if v is True ])
+			settings = { k: v for (k, v) in settings.items() if v is not True and v is not False }
+			return settings
 
-			lines.append(command)
+		settings_list = [ build_dict(common).items() for common in commons ]
 
-		lines.append("done")
+		lines.append(par_function)
+		lines.append(line)
+
+		def convert(d):
+			name = d[0][0]
+			return " ".join( str(v) for v in ["    :::", name] + list(set([ kv[1] for kv in d])) )
+
+
+		arr = [ convert(items) for items in zip(*settings_list) ]
+		parallel_args = " \\\n".join(arr)
+		lines.append(parallel_args)
+
+		# [print(l) for l in lines]
+
 		return lines
 
 	return { kv['name']: {num: func(commons, **kv)
