0275ea4a0638adb1c5cd18711891e8dad0c69fee
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-12-23 19:25:51 +0000

    use only one radius for the markov chain

--- scripts/create_commands/markov.py
+++ scripts/create_commands/markov.py
@@ -5,7 +5,7 @@ def create_commands(data, commons_grouped, place_dir, init_source, num_runs):
 
 	par_function = """
 Command=$( cat <<EOF
-place="{base_path}/out-{limit}-{races}-{cores}__{race_no}___{select_radius}_{influence_radius}_{chain_length}_{radius_as_percentage}";
+place="{base_path}/out-{limit}-{races}-{cores}__{race_no}___{influence_radius}_{influence_radius}_{chain_length}_{radius_as_percentage}";
 [ -d \$place ] \
 	&& echo "Not writing to \$place, it exists"
 	&& exit;
@@ -13,7 +13,7 @@ echo "output_dir is \$place \$(models_timeout {limit})";
 record_cp \$place/logs/log-{race_no} \\
 		../instancegen/mchain/chain_sampling.py cpu {limit} \\
 		--models_timeout=\$(models_timeout {limit}) \
-		--select_radius={select_radius} \\
+		--select_radius={influence_radius} \\
 		--influence_radius={influence_radius} \\
 		--chain_length={chain_length} \\
 		--radius_as_percentage={radius_as_percentage} \\
