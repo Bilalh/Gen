72a2783edd222aa91535db4fd8ea69b418e48498
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-12-25 00:45:33 +0000

    nsample data

--- scripts/create_commands/nsample.py
+++ scripts/create_commands/nsample.py
@@ -6,11 +6,14 @@ def create_commands(data, commons_grouped, place_dir, init_source, num_runs):
 	par_function = """
 Command=$( cat <<EOF
 place="{base_path}/out-{limit}-{races}-{cores}__{race_no}___{influence_radius}_{radius_as_percentage}";
+printf ".timeout 5000\\nINSERT OR REPLACE INTO nsample('method', 'essence', 'total_timeout', 'models_timeout', 'races', 'influence_radius', 'radius_as_percentage', 'run_no', 'output_dir') \
+	VALUES('nsample', '{essence}', '\$(total_normalised {limit})', '\$(models_timeout_normalised {limit})', '{races}', '{influence_radius}', '\$(to_bool {radius_as_percentage})', '{race_no}', '\$place');" \
+		| sqlite3 results/info.db;
 [ -d \$place ] \
 	&& echo "Not writing to \$place, it exists"
 	&& exit;
 echo "output_dir is \$place \$(models_timeout {limit})";
-record_cp \$place/logs/log-{race_no} \\
+#record_cp \$place/logs/log-{race_no} \\
 		../instancegen/mchain/nsampling.py cpu {limit} \\
 		--models_timeout=\$(models_timeout {limit}) \
 		--influence_radius={influence_radius} \\
