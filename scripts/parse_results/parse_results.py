8d1bb59e3f8d9ca9b1d04f931868c4da008ea2e8
Bilal Syed Hussain <bilalshussain@gmail.com>
2013-12-25 23:39:52 +0000

    surmising  the results

---
+++ scripts/parse_results/parse_results.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Bilal Syed Hussain
+from pathlib import Path
+from pprint import pprint, pformat
+import sqlite3
+
+
+def parse_results(base_str):
+	base = Path(base_str)
+	info_db = base / "results" / "Info.db"
+	conn = sqlite3.connect(str(info_db))
+	conn.row_factory = sqlite3.Row
+
+	i = 0
+
+	def do_method(method_name):
+		data = []
+		for row in conn.execute("SELECT * FROM {}".format(method_name) ):
+			# print(dict(row))
+			results_db = base / row['output_dir'] / "results.db"
+
+			results_conn = sqlite3.connect(str(results_db))
+			# results_conn.row_factory = sqlite3.Row
+			quality_row = results_conn.execute("SELECT min(quality) FROM  DiscriminatingParams Limit 1")
+			quality = list(quality_row)[0][0]
+			if not quality:
+				quality = 1.5
+			# print(quality)
+
+			row = dict(row)
+			del row['output_dir']
+			del row['quality']
+			results_conn.close()
+
+			nonlocal i
+			data.append({"x": i, "y": quality,  "parts": str(dict(row)) } )
+			i += 1
+
+		return data
+
+	methods_data = { k: do_method(k) for k in ["markov", "uniform", "nsample" ] }
+
+	return methods_data
+	conn.close()
+
+
+if __name__ == '__main__':
+	data = parse_results("/Users/bilalh/Desktop/Experiments")
+	pprint(data)
+
