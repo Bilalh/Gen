a7784066aeba965556c39c8e7744706821d0944c
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-01-07 20:52:53 +0000

    use multiprocessing to make script faster

--- scripts/parse_results/create_csv.py
+++ scripts/parse_results/create_csv.py
@@ -6,8 +6,14 @@ import sqlite3
 import csv
 import argparse
 
+import multiprocessing
+from multiprocessing import Queue
+import math
+import itertools as it
 
-def collect_data_as_dicts(base_str):
+from pprint import pprint
+
+def collect_data_as_dicts(base_str, num_proc):
 
     base = Path(base_str)
     info_db = base / "results" / "Info.db"
@@ -16,8 +22,10 @@ def collect_data_as_dicts(base_str):
     conn.row_factory = sqlite3.Row
 
     num_models = { row['essence']: row['num_models'] for row in conn.execute("Select * from essences")  }
-    rows = []
-    for row in conn.execute("SELECT * FROM everything"):
+    
+    def worker(rows_in, out_q):
+        rows = []
+        for row in rows_in:
             results_db = base / row['output_dir'] / "results.db"
             results_conn = sqlite3.connect(str(results_db))
             quality_row = results_conn.execute("SELECT min(quality) FROM  DiscriminatingParams Limit 1")
@@ -30,13 +38,32 @@ def collect_data_as_dicts(base_str):
             row_data['discriminating'] = discriminating_count
             row_data['num_models'] = num_models[row['essence']]
             rows.append(row_data)
+        out_q.put(rows)
+    
+    q_rows = list(conn.execute("SELECT * FROM everything"))
+    q_out = Queue()
+    chunksize = int(math.ceil(len(q_rows) / num_proc))
+    print("num_proc chunksize len(q_rows)", num_proc, chunksize,  len(q_rows))
+    procs = []
+    
+    for i in range(num_proc):
+        p = multiprocessing.Process(
+                target=worker,
+                args=(q_rows[chunksize * i:chunksize * (i + 1)],
+                      q_out))
+        procs.append(p)
+        p.start()
+    
+    results_rows = it.chain( *(q_out.get()  for i in range(num_proc)) )
 
-    return rows
+    keys = q_rows[0].keys() + ['quality', 'discriminating', 'num_models']
+
+    return (list(results_rows), keys)
 
 
-def write_dicts_as_csv(fp, rows):
+def write_dicts_as_csv(fp, rows, keys):
     with ( Path(fp) ).open("w") as f:
-        csv_writer = csv.DictWriter(f, delimiter=',', fieldnames=sorted(rows[0].keys()))
+        csv_writer = csv.DictWriter(f, delimiter=',', fieldnames=sorted(keys))
         csv_writer.writeheader()
         for r in rows:
             csv_writer.writerow(r)
@@ -44,10 +71,17 @@ def write_dicts_as_csv(fp, rows):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("base_dir")
+    parser.add_argument("num_proc", type=int)
+    parser.add_argument("all_results_fp")
+    parser.add_argument("base_dirs", nargs='+')
     args = parser.parse_args()
-    rows = collect_data_as_dicts(args.base_dir)
-    csv_fp = Path(args.base_dir) / "results" / "info.csv"
-    write_dicts_as_csv(csv_fp, rows)
+    pprint(args)
+    all_rows = []
+    for base_dir in args.base_dirs:
+        (rows, keys) = collect_data_as_dicts(base_dir, args.num_proc)
+        csv_fp = Path(base_dir) / "results" / "info.csv"
+        write_dicts_as_csv(csv_fp, rows, keys)
+        all_rows += rows
 
+    write_dicts_as_csv(args.all_results_fp, rows, keys)
 
