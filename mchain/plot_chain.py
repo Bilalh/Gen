c4928fd86ad76518c24fd232e023c807dd0f8875
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-01-17 13:09:13 +0000

    plot chain

--- mchain/plot_chain.py
+++ mchain/plot_chain.py
@@ -9,7 +9,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import random
 import json
 
-from lib.ncube import pick_inside_ncube, pick_inside_ncube2
+from lib.ncube import pick_inside
 
 
 def iter_many(it, length, num):
@@ -17,11 +17,12 @@ def iter_many(it, length, num):
         yield (it[i:i+1 + num])
 
 
-def plot_chain(current_chain, data_points):
+def plot_chain_2d(current_chain, data_points):
     # get_ipython().magic('matplotlib inline')
     plt.axis("equal")
 
     fig, ax = plt.subplots()
+    
 
     ax.scatter(*current_chain[0], c='b')
 
@@ -31,7 +32,8 @@ def plot_chain(current_chain, data_points):
     ax.plot(*list(zip(*current_chain)), c='g')
     ax.scatter(*list(zip(*current_chain[1:])), c='m')
     ax.scatter(*current_chain[0], c='b')
-    fig
+
+    fig.savefig('temp_2d.pdf', dpi=fig.dpi)
 
 
 def plot_chain_3d(current_chain, data_points):
@@ -52,7 +54,7 @@ def plot_chain_3d(current_chain, data_points):
 
     ax.scatter(*current_chain[0], c='b')
 
-    fig.savefig('temp.pdf', dpi=fig.dpi)
+    fig.savefig('temp_3d.pdf', dpi=fig.dpi)
 
 
 def plot_json_ouput(chain_fp, data_fp, limit=100):
@@ -62,7 +64,11 @@ def plot_json_ouput(chain_fp, data_fp, limit=100):
     else:
         data = []
 
-    plot_chain_3d(chain[0:limit], data[0:limit])
+    print(chain[0])
+    if len(chain[0]) == 3:
+        plot_chain_3d(chain[0:limit], data[0:limit])
+    else:
+        plot_chain_2d(chain[0:limit], data[0:limit])
 
 
 def test_ncube():
@@ -88,7 +94,7 @@ if __name__ == "__main__":
     [_, chain_fp, data_fp, limit] = sys.argv
     print([chain_fp, data_fp, limit])
     plot_json_ouput(chain_fp, data_fp, int(limit))
-    print("Saved temp.pdf")
+    print("fin")
 
 
 
