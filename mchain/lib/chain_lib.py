cb25d3c10110bd3a96a02eb981325e2c3d48d2ed
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-01-17 13:10:41 +0000

    misc

--- mchain/lib/chain_lib.py
+++ mchain/lib/chain_lib.py
@@ -150,6 +150,8 @@ def quality(count, minionTimeout, minionSatisfiable, minionSolutionsFound, isOpt
 	"""
 	logger.info("quality %s", (count, minionTimeout, minionSatisfiable, minionSolutionsFound, isOptimum, isDominated))
 
+	# should only be 1 -  (isDominated/ count)
+
 	if minionTimeout == 0:
 		return 1
 	elif minionTimeout == count:
