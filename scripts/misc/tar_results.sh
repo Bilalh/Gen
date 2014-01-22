ac8951f7ec88da4dfb1d366df9da46ce5c67a3d9
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-01-22 04:19:29 +0000

    script to tar results

---
+++ scripts/misc/tar_results.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -o nounset
+
+# Tar files so that I don't have too many files
+# and for convenience since they grouped by param
+
+mode=${1:-${MODE}}
+
+if [  -d "results-${mode}" ]; then
+	pushd "results-${mode}"
+else
+	echo "results-${mode} does not exist"
+	exit 2
+fi
+
+
+mkdir -p _stats
+
+Command=$( cat <<EOF
+echo "taring stats" &&
+tar czf _stats/{/.}.tar.gz \
+	\$(ls *{/.}* | egrep -v "minion$|minion.aux$")  
+EOF
+)
+
+Minions=$( cat <<EOF
+echo "taring minions" &&
+tar czf _stats/{/.}.minions.tar.gz \
+	\$(ls *{/.}* | egrep "minion$|minion.aux$") 
+EOF
+)
+
+Remove=$( cat <<EOF
+echo "removing" &&
+	rm \$(ls *{/.}* | egrep -v "zfinished")  
+EOF
+)
+
+
+ parallel --tagstring "{/.}" $Command ::: ../params/*
+
+ parallel --tagstring "{/.}" $Minions ::: ../params/*
+ 
+ parallel  --tagstring "{/.}" $Remove ::: ../params/*
+ 
+ popd
