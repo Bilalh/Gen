79922604a038208930c043d78cdb285ba21fb4f0
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-01-23 18:19:33 +0000

    tar -czf is supposedly better then tar czf   also compress logs since vim can read them each when compressed

--- scripts/misc/tar_results.sh
+++ scripts/misc/tar_results.sh
@@ -18,24 +18,26 @@ fi
 Tar_dir="_tar"
 mkdir -p ${Tar_dir}
 
+NUM_JOBS_M=$((${NUM_JOBS}/2))
+export NUM_JOBS_M
+
 Params=$( cat <<EOF
 echo "taring eprime-param" &&
-tar czf ${Tar_dir}/{/.}.params.tar.gz \
+tar -czf ${Tar_dir}/{/.}.params.tar.gz \
 	\$(ls *{/.}* | egrep  "eprime-param$")  
 EOF
 )
 
 Stats=$( cat <<EOF
 echo "taring stats" &&
-tar czf ${Tar_dir}/{/.}.stats.tar.gz \
+tar -czf ${Tar_dir}/{/.}.stats.tar.gz \
 	\$(ls *{/.}* | egrep -v "minion$|minion.aux$|eprime-param$")  
 EOF
 )
 
 Minions=$( cat <<EOF
 echo "taring minions" &&
-tar czf ${Tar_dir}/{/.}.minions.tar.gz \
-	\$(ls *{/.}* | egrep "minion$|minion.aux$") 
+tar -c \$(ls *{/.}* | egrep "minion$|minion.aux$")  | pigz -c -p${NUM_JOBS_M} > ${Tar_dir}/{/.}.minions.tar.gz
 EOF
 )
 
@@ -46,7 +48,6 @@ EOF
 )
 
 
-
 parallel -j${NUM_JOBS} --tagstring "finding finished {/.}" 'ls *{/.}*.zfinished > p-{/.}.finished' ::: ../params/*
 find . -name 'p-*.finished' -empty -delete
 
@@ -55,9 +56,15 @@ parallel -j${NUM_JOBS} --tagstring "{/.}" $Params ::: ../params/*
 
 parallel -j${NUM_JOBS} --tagstring "{/.}" $Stats ::: ../params/*
 
-parallel -j${NUM_JOBS} --tagstring "{/.}" $Minions ::: ../params/*
+parallel -j2 --tagstring "{/.}" $Minions ::: ../params/*
 
 # rm ? seems to not delete some files in parallel
 parallel -j1 --tagstring "{/.}" $Remove ::: ../params/*
 
+popd
+
+# vim can read compressed text files
+pushd "${place}"
+find  . -path '*/logs/*' -name 'log-*.log' | parallel  -j${NUM_JOBS} --tagstring "{/.}" "tar -cvzf  {}.tar.gz -C {//} {/}" 
+find  . -path '*/logs/*' -name 'log-*.log' -delete 
 popd
