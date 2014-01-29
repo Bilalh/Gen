524934bbfe3c20bcd58adf2a16505fb5b5405bbd
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-01-29 12:53:57 +0000

    fix condition

--- mchain/lib/method.py
+++ mchain/lib/method.py
@@ -170,7 +170,7 @@ class Method(metaclass=ABCMeta):
             conn = sqlite3.connect(os.path.join(self.output_dir, 'results.db'))
             results = [  (self.models_dir / row[0]).with_suffix('.eprime')
                         for row in conn.execute("SELECT eprime FROM EprimeOrdering") ]
-            if "LIMIT_MODELS" not in os.environ:
+            if "LIMIT_MODELS" in os.environ:
                 results = results[0:int(os.environ['LIMIT_MODELS'])]
             return "\n".join(map(str, results))
         else:
