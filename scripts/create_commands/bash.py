18bfe1623f8d7023d497ddb8fc4694d126133805
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-02-21 04:41:12 +0000

    better version detection

--- scripts/create_commands/bash.py
+++ scripts/create_commands/bash.py
@@ -26,8 +26,17 @@ function record_cp(){
 		if [ "`basename \"${name}\"`" != "${name}" ]; then
 			mkdir -p "`dirname \"${name}\"`"
 		fi
-		local fp="${name}-`date +%F_%H-%M_%s`"
+
+		local _fp="${name}-`date +%F_%H-%M_%s`"
+		fp="`python -c 'import os,sys; print(os.path.realpath(sys.argv[1]))'  ${_fp}`"
 		echo "pwd:`pwd`"> "${fp}.cmds"
+
+
+		if ( git rev-parse --is-inside-work-tree ); then
+			echo "repo(git) version" >> "${fp}.cmds"
+			git log -1 --format="%H" >> "${fp}.cmds"
+			git describe --always    >> "${fp}.cmds"
+		fi
 
 		echo "conjure">> "${fp}.cmds"
 		conjure 2>&1 | grep Version						  >> "${fp}.cmds"
@@ -35,15 +44,24 @@ function record_cp(){
 		savilerow	 | head -n 2 | tail -n 1			  >> "${fp}.cmds"
 
 		local sr="$(dirname `which savilerow`)"
-		if [ -f "${sr}/.hg/cache/branchheads" ]; then
-			cat "${sr}/.hg/cache/branchheads" >> "${fp}.cmds"
+		if [ -d "${sr}/.hg" ]; then
+			hg log -r . --template "{latesttag}-{latesttagdistance}-{node|short}
+" "${sr}" >> "${fp}.cmds"
 		fi
+
+		if [ -d "../instancegen/.git" ]; then
+			pushd "../instancegen/"
+			echo "instancegen(git) version" >> "${fp}.cmds"
+			git log -1 --format="%H" >> "${fp}.cmds"
+			git describe --always >> "${fp}.cmds"
+			popd
+		fi
+
 		echo "Command:"  >> "${fp}.cmds"
 		echo "$@"        >> "${fp}.cmds"
 		$@ 2>&1 | tee "${fp}.log"
 	fi
 }
-
 # only "True" is true
 function to_bool(){
 	if [ "$1" == "True" ]; then
