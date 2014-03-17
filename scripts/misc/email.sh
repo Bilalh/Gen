ff7679bb23ee41b209edab1453e31f6dacb424e0
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-03-17 22:21:17 +0000

    email script

--- scripts/misc/email.sh
+++ scripts/misc/email.sh
@@ -3,42 +3,99 @@
 function record_experiment_info(){
 	local _fp="$1"
 	local fp="`python -c 'import os,sys; print(os.path.realpath(sys.argv[1]))'  ${_fp}`"
-	echo "" > $fp
+	echo "" > "${fp}"
 	pushd /home/azureuser/repos/instancegen-models
 
-	hostname >> $fp
+	hostname >> "${fp}"
 
-	du -sch results/ >> $fp
-	echo "" >> $fp
+	du -sch results/ >> "${fp}"
+	echo "" >> "${fp}"
 
-	tree -L 4 results -I '*eprime*' -sh >> $fp
-	echo "" >> $fp
+	tree -L 4 results -I '*eprime*' -sh >> "${fp}"
+	echo "" >> "${fp}"
 
-	df -h >> $fp
-	echo "" >> $fp
+	df -h >> "${fp}"
+	echo "" >> "${fp}"
 
 	if ( ls results/setting*.json ); then
-		cat `ls results/setting*.json | tail -n1` >> $fp 
-		echo "" >> $fp
+		cat $(ls results/setting*.json | tail -n1) >> "${fp}"
+		echo "" >> "${fp}"
 	fi
 
 	popd
 
 	pushd /home/azureuser/repos/instancegen
-	git diff >> $fp
+	git diff >> "${fp}"
 	popd
 
-	echo "" >> $fp
-	echo "Bilal" >> $fp
+	pwd >> "${fp}"
+	if ( git rev-parse --is-inside-work-tree ); then
+		echo "repo(git) version" >> "${fp}"
+		git log -1 --format="%H" >> "${fp}"
+		git describe --always    >> "${fp}"
+
+		git diff >> "${fp}"
+	fi
+
+
+	if ( hg summary ); then
+		hg log -r . --template "{latesttag}-{latesttagdistance}-{node|short}" >> "${fp}"
+		hg summary >> "${fp}"
+
+		hg diff >> "${fp}"
+	fi
+
+	echo "" >> "${fp}"
+	echo "Bilal" >> "${fp}"
 }
 
 
 function notify_experiment_finished(){
-	local subject="[Experiments](`hostname`) Finished"
+	local subject="[Experiments](`hostname`) <`date +%F`>"
 	local address="bh246@st-andrews.ac.uk"
 	local fp=$(mktemp /tmp/email-file.XXXXXXXX)
-	record_experiment_info ${fp}
-	mail -s "${subject}" ${address} < $fp
+	record_experiment_info "${fp}"
+	mail -s "${subject}" ${address} < "${fp}"
 	echo "email sent to ${address}"
-	rm ${fp}
+	rm "${fp}"
 }
+
+function record_experiment_info2(){
+	local _fp="$1"
+	local fp="`python -c 'import os,sys; print(os.path.realpath(sys.argv[1]))'  ${_fp}`"
+	echo "" > "${fp}"
+
+	pwd >> "${fp}"
+	tree -L 2 >> "${fp}"
+	echo "" >> "${fp}"
+
+	if ( git rev-parse --is-inside-work-tree ); then
+		echo "repo(git) version" >> "${fp}"
+		git log -1 --format="%H" >> "${fp}"
+		git describe --always    >> "${fp}"
+
+		git diff >> "${fp}"
+	fi
+
+
+	if ( hg summary ); then
+		hg log -r . --template "{latesttag}-{latesttagdistance}-{node|short}" >> "${fp}"
+		hg summary >> "${fp}"
+
+		hg diff >> "${fp}"
+	fi
+
+	echo "" >> "${fp}"
+	echo "Bilal" >> "${fp}"
+}
+
+function notify_experiment_finished2(){
+	local subject="[Experiments](`hostname`) <`date +%F`>"
+	local address="bh246@st-andrews.ac.uk"
+	local fp=$(mktemp /tmp/email-file.XXXXXXXX)
+	record_experiment_info2 "${fp}"
+	mail -s "${subject}" ${address} < "${fp}"
+	echo "email sent to ${address}"
+	rm "${fp}"
+}
+
