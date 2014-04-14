d122c12dd50666aeec069b64b6c87351fbe5f39d
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-04-14 18:12:02 +0100

    connivence  script

---
+++ scripts/analyse_results/show_csv.r
@@ -0,0 +1,5 @@
+dirr   <- "/Users/bilalh/Desktop/Experiment/__minion_random/params_all_methods/prob034-Warehouse"
+sat   <- read.csv(paste(dirr,"sat-short.csv", sep='/' ))
+unsat <- read.csv(paste(dirr,"unsat-short.csv", sep='/' ))
+View(sat)
+View(unsat)
