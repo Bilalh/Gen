59dff22e0fa5c851a0a762147f56d65d313e1a74
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-04-15 19:13:35 +0100

    removed uneeded

--- scripts/misc/copy_minions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Bilal Syed Hussain
-
-set -o nounset
-cd /Volumes/Bs4Tb
-rsync -av --compress /Users/bilalh/CS/paramgen minions --include '*/' --include '*.minion' --exclude '*'
-find minions -empty -delete
