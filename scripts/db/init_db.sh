f02b3d6b4027d42cdcd152373159516992f808f5
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-05-07 00:23:19 +0100

    db convenience

--- scripts/db/init_db.sh
+++ scripts/db/init_db.sh
@@ -159,11 +159,12 @@ sqlite3 "${REPOSITORY_BASE}/results.db" <<SQL
         ;
 
 	CREATE VIEW IF NOT EXISTS ParamsData as
-    Select P.paramHash, P.param,  P.quality, P.ordering,
+	    Select P.paramHash, P.param, T.TotalTimeout as modelTimeout, T.timestamp ,  P.quality, P.ordering,
     	Cast(count(eprime) as Integer) as eprimes_count, Cast(max(D.MinionSatisfiable) as Integer) as Satisfiable, Cast(max(MinionSolutionsFound) as Integer) as MaxSolutions,
     	group_concat(D.eprime, ", ") as eprimes
     From ParamQuality P
     Join TimingsDomination D on P.paramHash = D.paramHash
+	Join Timeouts T on T.paramHash = D.paramHash
     Where isDominated = 0
     Group by P.paramHash
     Order by P.quality
