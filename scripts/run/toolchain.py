a3814b9756aabec2fbbdb9f6dd4f8b581b097bb4
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-06-26 05:00:15 +0100

    finished script

--- scripts/run/toolchain.py
+++ scripts/run/toolchain.py
@@ -10,6 +10,8 @@ import os
 import shlex
 import shutil
 import subprocess
+import math
+import tempfile
 from signal import alarm, signal, SIGALRM, SIGKILL
 
 from pprint import pprint
@@ -22,48 +24,75 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(format='%(name)s:%(lineno)d:%(funcName)s: %(message)s',
         level=logging.INFO)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("essence", help='')
-parser.add_argument("--out", required=True, help='')
-parser.add_argument("--timeout", required=True,type=int, help='CPU time for everything')
-parser.add_argument("--param", help='')
-args = parser.parse_args()
+parse_args = argparse.ArgumentParser()
+parse_args.add_argument("essence", help='')
+parse_args.add_argument("--outdir", required=True, help='')
+parse_args.add_argument("--timeout", required=True,type=int, help='CPU time for everything')
+parse_args.add_argument("--param", help='', default=None)
+args = parse_args.parse_args()
+
+outdir = Path(args.outdir)
+if not outdir.exists():
+    outdir.mkdir(parents=True)
+if not args.param:
+    essence_param = outdir.with_name("empty.param")
+    with open(str(essence_param),"w") as f:
+        f.write("language ESSENCE' 1.0")
+        f.write("\n$This Fine is empty")
+
+else:
+    essence_param = args.param
 
 essence          = Path(args.essence)
-eprime           = essence.with_suffix(".eprime")
-eprime_solution  = essence.with_suffix(".eprime-solution")
-essence_solution = essence.with_suffix(".solution")
-minion           = essence.with_suffix(".minion")
+eprime           = outdir / "0001-{}.eprime".format(essence_param.stem)
+eprime_param     = eprime.with_suffix(".eprime-param")
+eprime_solution  = eprime.with_suffix(".eprime-solution")
+eprime_info      = eprime.with_suffix(".eprime.info")
+essence_solution = eprime.with_suffix(".solution")
+minion           = eprime.with_suffix(".minion")
 
-out_json = Path(args.out)
-out_log = out_json.with_suffix(".output")
+
+out_json = eprime.with_name("results.json")
+out_log = eprime.with_name("results.output")
 timeout = args.timeout
 
 Results = namedtuple("results", "code cpu_time real_time timeout finished cmd")
 def run_with_timeout(timeout, cmd):
     code = 0
     finished = True
+    py_timeout=False
     try:
         date_start = datetime.utcnow()
         start_usr = os.times().children_user
         start_sys = os.times().children_system
-        output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, universal_newlines=True)
+        output = subprocess.check_output(cmd, timeout=timeout,
+                stderr=subprocess.STDOUT, universal_newlines=True)
+    except subprocess.TimeoutExpired as e:
+        output = e.output
+        finished = False
+        py_timeout=True
     except subprocess.CalledProcessError as e:
         output = e.output
         code = e.returncode
         finished = False
-    except subprocess.TimeoutExpired as e:
-        output = e.output
-        code=139
-        finished = False
-
     end_usr = os.times().children_user
     end_sys = os.times().children_system
     date_end=datetime.utcnow()
+
     diff = date_end - date_start
     cputime_taken = (end_usr - start_usr) + (end_sys - start_sys)
 
-    logger.info("Took %0.2f (%0.2f real), user %0.2f sys %0.2f for\n\t%s",
+    if "savilerow" in c:
+        with eprime_info.open() as f:
+            (m_timeout,m_total,sr_real)  = [float(l.split(":")[1]) for l in f.readlines()
+                    if l.split(":")[0] in {"MinionTimeOut", "MinionTotalTime", "SavileRowTotalTime" } ]
+            if int(m_timeout) == 1:
+                #because some killed processes don't return cputime
+                logger.warn("Adding %2.0f to cputime_taken(%2.0f) because of cpulimit timeout",
+                        m_total, cputime_taken)
+                cputime_taken+=m_total + sr_real  # Best we can do at this point
+
+    logger.info("Took %0.2f (%0.2f real), reported user %0.2f sys %0.2f for\n\t%s",
             cputime_taken, diff.total_seconds(),
             (end_usr - start_usr), (end_sys - start_sys), " ".join(cmd)  )
 
@@ -72,23 +101,28 @@ def run_with_timeout(timeout, cmd):
                   timeout=timeout, finished=finished,cmd=cmd)
            ,output)
 
-def get_process_children(pid):
-    p = Popen('ps --no-headers -o pid --ppid %d' % pid,
-            shell = True, stdout = PIPE, stderr = PIPE)
-    stdout, stderr = p.communicate()
-    return (int(p) for p in stdout.split())
-
 
 Conjure = """
 time conjure --mode compact  --in-essence "{essence}" --out-eprime "{eprime}"
 """
+PARAM_REFINE="""
+conjure
+    --mode       refineParam
+    --in-essence       {essence}
+    --in-eprime        {eprime}
+    --in-essence-param {essence_param}
+    --out-eprime-param {eprime_param}
+"""
 
 SR ="""
 time savilerow  -mode Normal
-    -in-eprime {eprime}
-    -out-minion {minion}
-    -out-solution {eprime_solution}
-    -run-minion minion
+-in-eprime                   {eprime}
+-in-param                    {eprime_param}
+-out-minion                  {minion}
+-out-solution                {eprime_solution}
+-out-info                    {eprime_info}
+-run-solver
+-solver-options '-cpulimit {itimeout}'
 """
 
 UP = """
@@ -98,15 +132,17 @@ time conjure
     --in-eprime             {eprime}
     --in-eprime-solution    {eprime_solution}
     --out-essence-solution  {essence_solution}
+    --essence-param         {essence_param}
+    --eprime-param          {eprime_param}
 """
 
 Vaildate= """
 time conjure --mode validateSolution
-        --in-essence {essence}
+        --in-essence  {essence}
         --in-solution {essence_solution}
 """
 
-cmds = [Conjure, SR, UP, Vaildate]
+cmds = [Conjure, PARAM_REFINE, SR, UP, Vaildate]
 results=[]
 outputs=[]
 total_cpu_time=0
@@ -114,6 +150,7 @@ total_real_time=0
 all_finished=True
 erroed = None
 for (i,cmd) in enumerate(cmds):
+    itimeout=int(math.ceil(timeout))
     c=shlex.split(cmd.format(**locals()))
     (res, output) = run_with_timeout(timeout, c)
 
@@ -143,8 +180,11 @@ with out_json.open("w") as f:
             total_cpu_time=total_cpu_time,
             total_real_time=total_real_time,
             finished=all_finished,
-            erroed=erroed)
+            erroed=erroed,
+            given_time=int(args.timeout))
     f.write(json.dumps(d, indent=True,sort_keys=True))
 
 with out_log.open("w") as f:
     f.write("\n".join(outputs))
+
+logger.info("total_cpu_time:%0.2f  total_real_time:%0.2f", total_cpu_time, total_real_time)
