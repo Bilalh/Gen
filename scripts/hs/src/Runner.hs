a0c2a039405401dcbd6d1263c7856cc2d11ba562
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-07-07 20:58:43 +0100

    parsing

--- scripts/hs/src/Runner.hs
+++ scripts/hs/src/Runner.hs
@@ -4,12 +4,12 @@ module Runner where
 
 
 import Data.Aeson(FromJSON(..),ToJSON(..))
-import Data.Maybe(fromMaybe)
 import Data.Map(Map)
 
 import GHC.Generics (Generic)
 import System.Environment(getEnv)
-import System.FilePath.Posix((</>))
+import System.FilePath((</>))
+import System.Directory(doesFileExist)
 import System.Process(rawSystem)
 
 import Data.Functor((<$>))
@@ -25,54 +25,75 @@ data StatusI = Success_ | Timeout_  | ErrorUnknown_ | NumberToLarge_
 data ResultI = ResultI {
          erroed          :: Maybe Int
         ,last_status     :: StatusI
-        ,result_dir      :: FilePath
         ,results         :: [CmdI]
         ,total_cpu_time  :: Float
         ,total_real_time :: Float
     } deriving(Show, Generic)
 
 data CmdI = CmdI {
-     rcode :: Int
-    ,cpu_time :: Float
+     rcode     :: Int
+    ,cpu_time  :: Float
     ,real_time :: Float
-    ,finished :: Bool
-    ,timeout :: Float
-    ,status_ :: StatusI
-    ,cmd :: [String]
+    ,finished  :: Bool
+    ,timeout   :: Float
+    ,status_   :: StatusI
+    ,cmd       :: [String]
 
 } deriving(Show, Generic)
 
+type SolveR  = SettingI SolveM
+type RefineR = SettingI RefineM
+data SettingI a = SettingI {
+         essence_    :: String
+        ,given_time_ :: Float
+        ,outdir_     :: FilePath
+        ,successful_ :: Bool
+        ,time_taken_ :: Float
+        ,data_       :: a
+    } deriving (Show, Generic)
+
+instance (FromJSON a) => FromJSON (SettingI a)
+instance (ToJSON a)   => ToJSON (SettingI a)
+
 instance FromJSON ResultI
-instance ToJSON ResultI
+instance ToJSON   ResultI
 
 instance FromJSON CmdI
-instance ToJSON CmdI
+instance ToJSON   CmdI
 
 instance FromJSON StatusI
-instance ToJSON StatusI
+instance ToJSON   StatusI
 
-newtype RefineR = RefineR (Map String CmdI) deriving Show
-instance FromJSON RefineR where
-  parseJSON val = RefineR <$> parseJSON val
+newtype RefineM = RefineM (Map String CmdI) deriving Show
+instance FromJSON RefineM where
+  parseJSON val = RefineM <$> parseJSON val
+instance ToJSON RefineM where
+    toJSON val =  toJSON val
 
-newtype SolveR = SolveR (Map String ResultI) deriving Show
-instance FromJSON SolveR where
-  parseJSON val = SolveR <$> parseJSON val
+newtype SolveM = SolveM (Map String ResultI) deriving Show
+instance FromJSON SolveM where
+  parseJSON val = SolveM <$> parseJSON val
+instance ToJSON SolveM where
+    toJSON val =  toJSON val
 
 
-getJSONResult :: FilePath ->  IO (Maybe ResultI)
-getJSONResult fp = fmap A.decode $ B.readFile fp
+getJSON :: FromJSON a => FilePath -> IO (Maybe a)
+getJSON fp = do
+    b <- doesFileExist fp
+    if b then
+        fmap A.decode $ B.readFile fp
+    else
+        return Nothing
 
-getJSONRefine :: FilePath ->  IO (Maybe RefineR)
-getJSONRefine fp = fmap A.decode $ B.readFile fp
-
-getJSONSolve :: FilePath ->  IO (Maybe SolveR)
-getJSONSolve fp = fmap A.decode $ B.readFile fp
-
-runToolChain :: FilePath -> FilePath -> Int -> IO ResultI
+runToolChain :: FilePath -> FilePath -> Int -> IO (Either RefineR (RefineR, SolveR ) )
 runToolChain spec dir timeou = do
     pg <- getEnv "PARAM_GEN_SCRIPTS"
     let toolchain= pg </> "run/toolchain.py"
-    _ <- rawSystem toolchain [spec, "--outdir", dir , "--timeout", show timeou]
-    result <- getJSONResult $ dir </> "result.json"
-    return $ fromMaybe (error "no result") result
+    _       <- rawSystem toolchain [spec, "--outdir", dir , "--timeout", show timeou]
+    refineF <- getJSON $ dir </> "refine_essence.json"
+    solveF  <- getJSON $ dir </> "solve_eprime.json"
+
+    return $ case (refineF, solveF) of
+        (Just r, Just s)  -> Right (r,s)
+        (Just r, Nothing) -> Left r
+        _                 -> error "Should not happen"
