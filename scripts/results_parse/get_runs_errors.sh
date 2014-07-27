0a50145483a439f7fd55b9542ff1c70f4add9ece
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-07-27 04:59:37 +0100

    spacing

--- scripts/results_parse/get_runs_errors.sh
+++ scripts/results_parse/get_runs_errors.sh
@@ -2,4 +2,5 @@
 set -o nounset
 
 
-find ${1} -name state.json | parallel -j${NUM_JOBS:-4} "$PARAM_GEN_SCRIPTS/results_parse/get_runs_errors.py {} summary"
+find ${1} -name state.json | parallel -j${NUM_JOBS:-4} \
+	"$PARAM_GEN_SCRIPTS/results_parse/get_runs_errors.py {} summary"
