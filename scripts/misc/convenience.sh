41c028465528c838753e604ff300f21ac356b495
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-10-16 15:06:26 +0100

    function

--- scripts/misc/convenience.sh
+++ scripts/misc/convenience.sh
@@ -280,3 +280,95 @@ function vaildate_solution(){
 # 			return 1
 # 		fi
 # }
+
+function save_versions(){
+    local _fp="$1$(date +%F_%H-%M_%s)"
+	local fp="`python -c 'import os,sys; print(os.path.realpath(sys.argv[1]))'  ${_fp}`"
+	echo "" >> "${fp}.versions"
+	
+	if [ ! -f "${fp}.versions" ] ; then
+		echo "Can't write to ${fp}.versions"
+		return
+	fi
+
+	if ( git rev-parse --is-inside-work-tree ); then
+		echo "repo(git) version" >> "${fp}.versions"
+		git log -1 --format="%H" >> "${fp}.versions"
+		git describe --always    >> "${fp}.versions"
+		echo "" >> "${fp}.versions"
+	fi
+
+
+	local repos_dir="$(dirname `which savilerow`)/../"
+	
+	
+
+
+	echo "conjure" >> "${fp}.versions"
+	conjure 2>&1 | grep Version  >> "${fp}.versions"
+    echo "" >> "${fp}.versions"
+	minion 2>&1	 | egrep 'HG version|Minion Version' >> "${fp}.versions"
+    echo "" >> "${fp}.versions"
+	savilerow	 | grep Version >> "${fp}.versions"
+    echo "" >> "${fp}.versions"
+
+	echo ""  >> "$fp.versions"
+	echo "##Repos##"  >> "$fp.versions"
+	echo ""  >> "$fp.versions"
+	
+	declare -a repos 
+	repos=(conjure, conjure-private conjure-testing essenceAST instancegen instancegen-models minion) 
+	for repo in "${repos[@]}"; do
+
+		if [ ! -d "$repos_dir/$repo" ]; then
+			echo "$repos_dir/$repo" 
+			continue
+		fi
+		pushd "$repos_dir/$repo"
+		
+		if [ -d ".git" ] ; then
+			echo "$repo(git) version" >> "${fp}.versions"
+			git log -1 --format="%H" >> "${fp}.versions"
+			git describe --always    >> "${fp}.versions"
+			echo "" >> "${fp}.versions"
+		elif [ -d ".hg" ]; then
+			echo "$repo(hg) version" >> "${fp}.versions"
+			hg log -r . --template "{latesttag}-{latesttagdistance}-{node|short}" >> "${fp}.versions"
+			echo "" >> "${fp}.versions"
+		fi 
+		
+	    echo "" >> "${fp}.versions"
+		popd
+	done
+	
+	
+
+	echo "##OTHER##"  >> "$fp.versions"
+	echo "" >> "${fp}.versions"
+
+	declare -a commands
+    commands=(python python3 pip sqlite3 git hg perl parallel ruby ghc pigz)
+	for prog in "${commands[@]}"; do
+		echo "$prog version:" >> "${fp}.versions"
+		$prog --version  2>&1 | cat | head -n3 >> "${fp}.versions"
+		echo "" >> "${fp}.versions"
+	done
+
+	# Java just had to be different
+	echo "java version:" >> "${fp}.versions"
+	java -version  2>&1 | cat | head -n3 >> "${fp}.versions"
+    echo "" >> "${fp}.versions"
+
+
+	echoing pip freeze  2>&1  >> "${fp}.versions" 
+    echo "" >> "${fp}.versions"
+
+
+	echoing pip3 freeze  2>&1   >> "${fp}.versions" 
+    echo "" >> "${fp}.versions"
+	 
+	echoing cabal list --installed --simple-output  2>&1  >> "${fp}.versions" 
+    echo "" >> "${fp}.versions"
+
+
+}
