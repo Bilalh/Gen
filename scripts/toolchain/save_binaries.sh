865f03d826fb775807ef1bc13935bcdd0aa86791
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-10-24 02:31:24 +0100

    save binaries

--- scripts/toolchain/save_binaries.sh
+++ scripts/toolchain/save_binaries.sh
@@ -12,9 +12,11 @@ _dir="$1$(date +%F_%H-%M_%s)/"
 mkdir -p "$_dir"
 dir="$(python -c 'import os,sys; print(os.path.realpath(sys.argv[1]))'  "${_dir}")"
 
-"$SCRIPT_DIR/save_version.sh" "$dir/_versions"
+"$SCRIPT_DIR/save_version.sh" "$dir/versions@"
 
-cd "$dir"
+pushd "$dir"
+mkdir binaries
+pushd binaries
 
 repos_dir="$(dirname "$(which savilerow)")/../"
 
@@ -24,3 +26,9 @@ cp "$(which savilerow)" .
 cp "$(which minion)" .
 cp "$repos_dir/savilerow/savilerow.jar" .
 cp -r "$repos_dir/instancegen/scripts/hs/bin" ig_bin
+
+popd
+tar -c binaries | pigz -c -p ${NUM_JOBS:-4} > binaries.tar.gz
+rm -rf binaries
+
+popd
