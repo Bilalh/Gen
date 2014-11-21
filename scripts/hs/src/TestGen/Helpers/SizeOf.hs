308dd4f34744c7266fd9e66da288604ea4f00592
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-11-21 00:30:15 +0000

    bounded version

--- scripts/hs/src/TestGen/Helpers/SizeOf.hs
+++ scripts/hs/src/TestGen/Helpers/SizeOf.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE QuasiQuotes, OverloadedStrings, ViewPatterns #-}
 {-# LANGUAGE NamedFieldPuns, RecordWildCards #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, MultiWayIf #-}
 
 module TestGen.Helpers.SizeOf where
 
@@ -18,12 +18,60 @@ class DepthOf a where
     depthOf :: a -> Integer
 
 
+-- How many different unique values are in say a domain
+-- return the given values if the a is large then it 
+class SizeOfBounded a where
+    sizeOfBounded:: Integer -> a  -> Integer
+
+
+
 instance SizeOf Domain where
     sizeOf DInt{..} = fromIntegral .  S.size .  S.fromList . concatMap rangeInts $ ranges
     sizeOf r = docError [
         "sizeOf not matched",
         pretty $ show r, pretty r
         ]
+
+-- instance SizeOf Type where
+--     -- FIXME hardcoded
+--     sizeOf TInt            = 20
+--     sizeOf TBool           = 2
+--     sizeOf (TMatix inner)  = 20 ^ (sizeOf inner)
+--     sizeOf (TSet inner)    = 2  ^ (sizeOf inner)
+--     sizeOf (TMSet inner)   = 2  ^ (sizeOf inner)
+--     sizeOf (TFunc from to) = ((sizeOf to) + 1 )  ^ (sizeOf from)
+--     sizeOf (TTuple inners) = product (map sizeOf inners )
+--     sizeOf (TRel inners)   = sizeOf (TSet (TTuple inners) )
+--     sizeOf (TPar inner)    = sizeOf (TSet (TSet inner))
+--     sizeOf TAny            = error "SizeOf Type called with TAny"
+--     sizeOf (TUnamed _)     = error "SizeOf Type called with TUnamed"
+--     sizeOf (TEnum _)       = error "SizeOf Type called with TUnamed"
+
+instance SizeOfBounded Type where
+    -- FIXME hardcoded
+    sizeOfBounded m TInt           = 20   
+    sizeOfBounded m TBool          = 2 
+    sizeOfBounded m (TMatix inner) = minB m (sizeOfBounded m inner) (20 ^)
+    sizeOfBounded m (TSet inner)   = minB m (sizeOfBounded m inner) (2 ^)
+    sizeOfBounded m (TMSet inner)  = minB m (sizeOfBounded m inner) (2 ^)
+    sizeOfBounded m (TTuple inners)= min m ( product (map (sizeOfBounded m) inners ) )
+    sizeOfBounded m (TRel inners)  = sizeOfBounded m (TSet (TTuple inners) )
+    sizeOfBounded m (TPar inner)   = sizeOfBounded m (TSet (TSet inner))
+    sizeOfBounded m TAny           = error "SizeOf Type called with TAny"
+    sizeOfBounded m (TUnamed _)    = error "SizeOf Type called with TUnamed"
+    sizeOfBounded m (TEnum _)      = error "SizeOf Type called with TUnamed"
+    
+    sizeOfBounded m (TFunc from to) = 
+        let toSize   = sizeOfBounded m to 
+            fromSize = sizeOfBounded m from
+        in if
+           | (toSize + 1) >= m -> m
+           | otherwise         -> min m ( (toSize + 1) ^ (fromSize) )
+    
+
+
+minB m v _ | v >= m = m 
+minB m v f          = min m (f v)
 
 
 instance DepthOf Type where
