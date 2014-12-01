4c55dbe431851cf4e6778c33f3e096b40b0d98d8
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-12-01 21:47:46 +0000

    for testing

---
+++ scripts/hs/src/TestGen/QCDebug.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE QuasiQuotes, OverloadedStrings, ViewPatterns #-}
+
+{-# LANGUAGE RecordWildCards, NamedFieldPuns, ScopedTypeVariables #-}
+{-# OPTIONS_GHC -fno-cse #-} 
+-- cse means output is not outputted
+
+module TestGen.QCDebug where
+
+import TestGen.Arbitrary.Arbitrary
+import TestGen.Arbitrary.Domain
+import TestGen.Arbitrary.Type(atype_only)
+import TestGen.Prelude
+
+import qualified Data.Map as M
+import qualified Data.Text as T
+import qualified Test.QuickCheck as QC
+import qualified Test.QuickCheck.Property as QC
+
+import Text.Groom(groom)
+
+
+newtype S1 = S1 SpecE
+    deriving Show  
+
+instance Arbitrary S1 where
+    arbitrary = arbitraryDef undefined
+
+instance ArbSpec S1 where
+    getSpec (S1 sp) = sp
+    wrapSpec sp     = S1 sp
+    
+    tyGens _ = def{
+          gen_atype = atype_only [ TBool, TSet TAny  ]
+        , gen_dom   = dom_only [boolDomChoice]
+        , gen_useFunc = myUseFunc
+        }
+
+myUseFunc :: FuncsNames -> Bool 
+myUseFunc Aunion = True
+myUseFunc _      = False
