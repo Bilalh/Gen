26b9a1a8283fe0c17158b3037d2332a42ca1abe0
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-12-11 02:02:35 +0000

    log all output

--- scripts/hs/scripts/testSampleWrap.sh
+++ scripts/hs/scripts/testSampleWrap.sh
@@ -8,9 +8,11 @@ echo "testSample before ${testSample:-}"
 export testSample=${testSample:-testSample}
 
 mkdir -p "$base"
-"$PARAM_GEN_SCRIPTS/toolchain/save_version.sh" "${base}/"
+dat="$(date +%F_%H-%M_%s)"
+mkdir "${base}/${dat}"
+
+"$PARAM_GEN_SCRIPTS/toolchain/save_version.sh" "${base}/${dat}/zver@"
 cores=${CORES:-"$(parallel --number-of-cores)"}
 
 
-
-${testSample}  -b "${base}/$(date +%F_%H-%M_%s)" --cores "$cores" "$@" ;
+${testSample}  -b "${base}/${dat}" --cores "$cores" "$@" 2>&1 | tee "${base}/${dat}/_all.logged" ;
