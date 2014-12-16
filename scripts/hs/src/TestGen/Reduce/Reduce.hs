f4b22351cd983ba922e4518c2365608101ce3e97
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-12-16 01:34:45 +0000

    some test case reduction
    from
    find var1: set of bool
    such that {true} = {true, true} \/ false
    
    to
    find var1: set of bool
    such that {true} = {true, true}

--- scripts/hs/src/TestGen/Reduce/Reduce.hs
+++ scripts/hs/src/TestGen/Reduce/Reduce.hs
@@ -22,15 +22,27 @@ import TestGen.QCDebug(specE1)
 import System.Random(randomIO)
 import Control.Arrow((&&&))
 
+import TestGen.Helpers.Runner
+
+import System.Directory(createDirectoryIfMissing, removeDirectoryRecursive)
+
 class Reduce a where
-    reduce :: a -> [a]
-    single :: a -> [Expr]
+    reduce   :: a -> [a]    -- list of smaller exprs
+    single   :: a -> [Expr] -- smallest literal e.g  [true, false] for  a /\ b
+    subterms :: a -> [Expr] -- a /\ b  -->   [a, b]
+
+    -- reduce a   = error "no default reduce"
+    -- single a   = error "no default of single"
+    -- subterms a = error "no default of subterms"
 
 instance Reduce Expr where
-    reduce (EBinOp op) = single op ++ map EBinOp (reduce op) 
-    reduce b = [] -- no reductions possible   
-
-    single a = error "no default of single"
+    reduce (EBinOp op) = single op ++ subterms op ++ map EBinOp (reduce op) 
+    
+    reduce a   = [] -- no reductions possible   
+    
+    single a   = error "no single expr"
+    subterms a = [] 
+    
 
 instance Reduce BinOp where
     reduce (BOr a b) = map ( uncurry BOr ) $  catMaybes
@@ -38,11 +50,23 @@ instance Reduce BinOp where
         , (etrue,b)  *| simpler etrue a , (efalse, b) *| simpler efalse a ]
 
     reduce b = []
-    single b = [etrue,  efalse]
+    
+    single (BOr _ _) = [etrue,  efalse]
+    single (BEQ _ _) = [etrue,  efalse]
+    
+    single a = error . show . vcat   
+        $ ["single missing case", pretty $ toEssence a, pretty $ groom a ]
+    
+
+    subterms (BOr a b) = [a,b] 
+    subterms (BEQ a b) = [a,b] 
+    
+
+    subterms a = error . show . vcat   
+        $ ["subterms missing case", pretty $ toEssence a, pretty $ groom a ]
 
 etrue  = ELit (EB True)
 efalse = ELit (EB False)
-
 
 
 reduceMain :: SpecE -> IO SpecE 
@@ -124,47 +148,52 @@ removeUnusedDomain sp = undefined
 
 -- True means error still happens
 runSpec :: SpecE -> IO Bool
-runSpec sp = do
-    stillErroed :: Bool <- randomIO 
+runSpec spE = do
+    let sp = toSpec spE
+    let path = "/Users/bilalh/CS/break_conjure/fixed/46c3d2b43f4e/2014-12-10_02-01_1418176894/_errors/Validate_/ErrorUnknown_/1418178864_89/out"
+    
+    -- removeDirectoryRecursive breaks if dir eixsts
+    createDirectoryIfMissing True path >> removeDirectoryRecursive path
+    
+    res <- runToolchain' 33 4 sp (path) 120 True
+    let stillErroed  = sameError res
+     
     print $ (stillErroed, pretty sp)
     return stillErroed
 
-
-_reduce e = 
-    case  fromEssence e of 
-        Left err -> error . show .  (pretty &&& pretty . groom)  $ err
-        Right ee -> do
-            let res = reduce ee
-            mapM (print  . pretty . toEssence)  res
-            return res
-
-
--- a r= a r +1
---
--- ts = [1, 2,3,a 4]
---
--- func tt =
---     let a = tt !! 0
---         b = tt !! 3
---         c = [b, a]
---     in c !! 1
-
+    where 
+    -- This would be a different error
+    sameError (Left SettingI{successful_=False}) = False
+    sameError (Right (_, SettingI{successful_=False,data_=SolveM ms })) = 
+        
+        let
+            f ResultI{last_status, erroed= Just index, results } = 
+                let kind = kind_ (results !! index)
+                in kind
+        
+            kinds = M.toList $  M.map f ms
+        in any (\(name,kind) -> kind == Validate_) kinds
+ 
+    sameError _ = False
 
 -- True if a1 is less simpler then a2
 class Simpler a b where
     simpler :: (ToEssence a, Eq a, ToEssence b, Eq b) => a -> b -> Bool
 
+-- instance Simpler a b where
+--     simpler _ _ = True
+
 instance Simpler Expr Expr where
     simpler (ELit a ) (ELit b)   = simpler a b
-    simpler (ELit a)  (EBinOp b) = simpler a b 
-    
+    simpler (ELit a)  (EBinOp b) = simpler a b
+
     -- simpler _ _ = False
     simpler a b = error . show . vcat . map (pretty)  $ [toEssence a, toEssence b ]
 
 instance Simpler Literal Literal where
     simpler (EB _) (EB _) = False
     simpler (EB _) _      = True
-    
+
     -- simpler _ _ = False
     simpler a b = error . show . vcat . map (pretty)  $ [toEssence a, toEssence b ]
 
@@ -174,13 +203,21 @@ instance Simpler Literal BinOp where
 
     -- simpler _ _ = False
     simpler a b = error . show . vcat . map (pretty)  $ [toEssence a, toEssence b ]
-    
+
 
 infixl 1 *|
 (*|) :: a -> Bool -> Maybe a
 a  *| c | c = Just a
 _  *| _    = Nothing
 
+
+_reduce e = 
+    case  fromEssence e of 
+        Left err -> error . show .  (pretty &&& pretty . groom)  $ err
+        Right ee -> do
+            let res = reduce ee
+            mapM (print  . pretty . toEssence)  res
+            return res
  
 _e :: FromEssence a => E -> a
 _e e =  case fromEssence e of 
