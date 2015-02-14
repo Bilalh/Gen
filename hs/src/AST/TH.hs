d19c37a97f9813e1ea4030de51d287333a1c3408
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-02-14 17:46:39 +0000

    for testing

---
+++ hs/src/AST/TH.hs
@@ -0,0 +1,62 @@
+module AST.TH(domain) where
+
+import Conjure.Language.Definition
+
+import Conjure.Prelude
+import Conjure.Language.Domain
+import Conjure.Language.Parser
+
+import Text.Parsec ( SourcePos, setPosition )
+import Text.Parsec.Pos ( newPos )
+
+import Language.Haskell.TH ( Q, runIO, Loc(..), location, mkName, ExpQ, varE, appE, PatQ, varP )
+import Language.Haskell.TH.Quote ( QuasiQuoter(..), dataToExpQ, dataToPatQ )
+
+import Data.Generics.Aliases ( extQ )
+
+domain :: QuasiQuoter
+domain = QuasiQuoter
+    { quoteExp = \ str -> do
+        l <- locationTH
+        e <- runIO $ parseIO (setPosition l *> parseDomain) str
+        dataToExpQ (const Nothing `extQ` expE `extQ` expD `extQ` expAP) e
+    , quotePat  = \ str -> do
+        l <- locationTH
+        e <- runIO $ parseIO (setPosition l *> parseDomain) str
+        dataToPatQ (const Nothing `extQ` patE `extQ` patD `extQ` patAP) e
+    , quoteType = error "quoteType"
+    , quoteDec  = error "quoteDec"
+    }
+
+locationTH :: Q SourcePos
+locationTH = aux <$> location
+    where
+        aux :: Loc -> SourcePos
+        aux loc = uncurry (newPos (loc_filename loc)) (loc_start loc)
+
+expE :: Expression -> Maybe ExpQ
+expE (ExpressionMetaVar x) = Just [| $(varE (mkName x)) |]
+expE _ = Nothing
+
+expD :: Domain () Expression -> Maybe ExpQ
+expD (DomainMetaVar x) = Just $ [| $(varE (mkName "forgetRepr")) |]
+                         `appE` [| "TH:expD" |]
+                         `appE` [| $(varE (mkName x)) |]
+expD _ = Nothing
+
+expAP :: AbstractPattern -> Maybe ExpQ
+expAP (AbstractPatternMetaVar x) = Just [| $(varE (mkName x)) |]
+expAP _ = Nothing
+
+
+patE :: Expression -> Maybe PatQ
+patE (ExpressionMetaVar x) = Just (varP (mkName x))
+patE _ = Nothing
+
+patD :: Domain () Expression -> Maybe PatQ
+patD (DomainMetaVar x) = Just (varP (mkName x))
+patD _ = Nothing
+
+patAP :: AbstractPattern -> Maybe PatQ
+patAP (AbstractPatternMetaVar x) = Just (varP (mkName x))
+patAP _ = Nothing
