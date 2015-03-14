d96dd0ed8a76d3cb3fac2ea7c7934d35dce5cbdd
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-03-14 04:29:59 +0000

    more tests

--- hs/src/test/Gen/ReduceTest.hs
+++ hs/src/test/Gen/ReduceTest.hs
@@ -13,35 +13,42 @@ import Test.Tasty.HUnit         (testCase, (@?=))
 import Test.Tasty.QuickCheck    as QC
 
 
-
 use_qc :: [Maybe a] -> [Maybe a]
 -- use_qc = return []
 use_qc xs = xs
-
 no _ = Nothing
 
 
 tests :: TestTree
-tests = testGroup "simpler"
+tests = testGroup "reduce"
 
-  [ testGroup "Expr_gen"
-   [
-      r_depth_leq [essencee| false |]
-    , r_depth_leq [essencee| false \/ false |]
-    , r_depth_leq [essencee| false != true |]
-    , r_depth_leq [essencee| 4 = -5 |]
-    , r_depth_leq [essencee| function(0 --> 3) = function(1 --> 7, 10 --> 6) |]
-    , r_depth_leq [essencee| partition({4}) != partition({7}, {3}) |]
-    , r_depth_leq [essencee| {true} = {true,true} |]
-    , r_depth_leq [essencee| (true \/ true) != true |]
-    , r_depth_leq [essencee| 10 = 7 \/ (7 % 10 = 3) |]
-    , r_depth_leq [essencee| 2 != 2 /\ (true \/ true) |]
-    , r_depth_leq [essencee| preImage(function(true --> true), false) |]
-    , r_depth_leq [essencee| toInt(toInt(true) in mset(-5, 4)) = 9 |]
-   ]
-
+  [ testGroup "Expr Gen"
+    [
+      testGroup "DepthOf <=" (map r_depth_leq gen_exprs)
+    , testGroup "Simpler <=" (map r_simp_leq gen_exprs)
+    , testGroup "TypeOf <="  (map r_type_leq gen_exprs)
+    , testGroup "Reducing Single can not decrease the depth "  (map r_single_depth gen_exprs)
+    ]
 
   ]
+
+gen_exprs :: [Expr]
+gen_exprs =
+    [ [essencee| false |]
+    , [essencee| false \/ false |]
+    , [essencee| false != true |]
+    , [essencee| 4 = -5 |]
+    , [essencee| function(0 --> 3) = function(1 --> 7, 10 --> 6) |]
+    , [essencee| partition({4}) != partition({7}, {3}) |]
+    , [essencee| {true} = {true,true} |]
+    , [essencee| (true \/ true) != true |]
+    , [essencee| 10 = 7 \/ (7 % 10 = 3) |]
+    , [essencee| 2 != 2 /\ (true \/ true) |]
+    , [essencee| preImage(function(true --> true), false) |]
+    , [essencee| toInt(toInt(true) in mset(-5, 4)) = 9 |]
+    ]
+
+
 
 __runner :: forall a t. (t -> StateT EState Identity a) -> t -> a
 __runner f ee = do
@@ -51,10 +58,33 @@ __runner f ee = do
       res             = runIdentity $ flip evalStateT state $ f ee
   res
 
-st :: (Simpler a b) => Ordering -> a -> b -> TestTree
-st ord a b = testCase (show $ pretty a <+> pretty b) $
-  (runIdentity $ simpler a b) @?= ord
 
 r_depth_leq :: Expr -> TestTree
 r_depth_leq a = testCase (show $ pretty a) $
           ( all (\b -> depthOf b <= depthOf a )  $ __runner reduce a ) @?= True
+
+
+r_simp_leq :: Expr -> TestTree
+r_simp_leq a = testCase (show $ pretty a) $
+          ( all (\b -> simpler_leq a b )  $ __runner reduce a ) @?= True
+
+r_type_leq :: Expr -> TestTree
+r_type_leq a = testCase (show $ pretty a) $
+          ( all (\b -> simpler_leq (runIdentity . ttypeOf $ a) (runIdentity . ttypeOf $ a) )
+                    $ __runner reduce a ) @?= True
+
+
+r_single_depth :: Expr -> TestTree
+r_single_depth a =
+    let reduced = __runner single a
+        minDepth = minimum $ map depthOf reduced
+        res  = map (\rr -> (all (\l -> depthOf l == depthOf rr  )  $ __runner reduce rr  )  )
+               $ reduced
+
+    in testCase (show $ pretty a) $ (and res)  @?= True
+
+
+
+simpler_leq :: forall a b. Simpler a b => a -> b -> Bool
+simpler_leq a b = let res = (runIdentity $ simpler a b)
+                  in  (res == EQ || res == LT)
