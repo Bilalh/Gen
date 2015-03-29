50b9320bfdbebe743286affb2f85496f137f470e
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-03-29 21:52:54 +0100

    reduce error script

---
+++ toolchain/reduce_error.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import json
+import sys
+import shlex
+import subprocess
+# from toolchain_recheck import data_convert_back
+
+import logging
+import argparse
+from pathlib import Path
+logger = logging.getLogger(__name__)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("essence", help='essence file')
+parser.add_argument('-o',  dest='output',       help='output dir', required=True)
+parser.add_argument('-p',  dest='total_time',   help='time per run', required=True)
+parser.add_argument('-bin_dir',  dest='bin_dir',   help='bin-dir' )
+
+
+args = parser.parse_args()
+
+essence = Path(args.essence)
+essence_dir = essence.parent
+
+if (essence_dir / "solve_eprime.json").exists():
+	data = json.load(open(essence_dir / "solve_eprime.json"))
+elif (essence_dir / "refine_essence.json").exists():
+	with (essence_dir / "refine_essence.json").open() as f:
+	    data=json.load(f)
+
+
+	for (names, vals) in data['data_'].items():
+		kind   = vals['kind_']
+		status = vals['status_']
+		if status == "ErrorUnknown_":
+			status = "StatusAny_"
+
+		cmd_str="""
+			gen reduce {essence_dir} -o '{output}' -p {total_time} -N --kind {kind} --status {status}
+		""".format(essence_dir=essence_dir, kind=kind, status=status,
+			       total_time=args.total_time, output=args.output)
+
+		if 'choices_json' in vals['vals']:
+			cs=Path(vals['vals']['choices_json']).name
+			cmd_str += " --choices {}".format(essence_dir / cs)
+
+		if args.bin_dir:
+			cmd_str += " --bin-dir {}".format(args.bin_dir)
+
+		cmd_arr=shlex.split(cmd_str)
+		try:
+			subprocess.check_call(cmd_arr)
+		except subprocess.CalledProcessError as e:
+			print("error")
+			sys.exit(e.returncode)
+
+
+else:
+	print("no solve_eprime.json or refine_essence.json file found")
+	sys.exit(3)
+
