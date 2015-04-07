3a9800664b5e748cf1a73b898d30d501d4fee4fb
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-04-07 18:53:38 +0100

    why was this there?

--- hs/scripts/clean.sh
+++ hs/scripts/clean.sh
@@ -7,4 +7,4 @@ find . -name "*.hi" -delete
 find . -name "*.o" -delete
 find . -name "*.hi-boot" -delete
 find . -name "*.o-boot" -delete
-rm -rf dist ~/.cabal-sandbox cabal.sandbox.config
+rm -rf dist .cabal-sandbox cabal.sandbox.config
