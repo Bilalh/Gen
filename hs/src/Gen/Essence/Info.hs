54bb5b5418c04b7da5c7a078ba5902ff17edb731
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-01 03:06:56 +0100

    Other idea

--- hs/src/Gen/Essence/Info.hs
+++ hs/src/Gen/Essence/Info.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable, DeriveFoldable, DeriveFunctor, DeriveGeneric,
              DeriveTraversable, FlexibleInstances, KindSignatures,
-             MultiParamTypeClasses #-}
+             MultiParamTypeClasses, QuasiQuotes #-}
 module Gen.Essence.Info where
 
 -- Idea for Generic Genration of ops
@@ -9,11 +9,15 @@ module Gen.Essence.Info where
 
 import Conjure.Language.Expression.Op.Internal.Generated (Op (..))
 import Conjure.Language.Expression.Op.Mod
+import Conjure.Language.Expression.Op.Geq
+import Conjure.Language.Expression.Op.Or
 import Conjure.Language.Type
 import Conjure.Language.Definition
 import Conjure.Prelude
--- import Data.Map                                          (Map)
+import Data.Map                                          (Map)
+import qualified Data.Map as M
 import Gen.AST.Imports
+import Gen.AST.TH
 
 
 newtype TyId = TyId Int
@@ -25,8 +29,7 @@ data Ty = Ty     TyId Type
         -- | TyDep  TyId (Map Type Type)  -- Needed?
   deriving (Eq, Ord, Show, Data, Typeable, Generic)
 
-
-class OpInfo a where
+class Show a => OpInfo a where
     artity      :: a -> Int
     minDepth    :: a -> Int
     argsTypes   :: a -> Type -> [Ty]
@@ -34,15 +37,6 @@ class OpInfo a where
 class ApplyOp a e where
   applyArgs ::  (a e -> Op e) -> [e] -> (Op e)
 
-class (Monad m, Applicative m) => MonadGen m where
-    --   0    Never chosen
-    --   100  Normal chance
-    --  >100  Incressed chance
-    getOpWeighting :: (a e -> Op e) -> m Int
-
-
-instance MonadGen Identity where
-    getOpWeighting _ =  return 100
 
 instance OpInfo (OpMod e -> Op e) where
     artity _            =  2
@@ -52,14 +46,50 @@ instance OpInfo (OpMod e -> Op e) where
 instance Show (OpMod e -> Op e) where
     show _ = "(OpMod e -> Op e)"
 
-instance ApplyOp (OpMod) e where
+instance ApplyOp OpMod e where
     applyArgs f [e1,e2] =  f (OpMod e1 e2)
 
+instance OpInfo (OpGeq e -> Op e) where
+    artity _             =  2
+    minDepth _           =  1
+    argsTypes _ TypeBool =  [ Ty (TyId 1) TypeInt, Ty (TyId 2) TypeInt ]
 
-allowdOpsForType :: MonadGen m
-                 => Type
-                 -> m [OpMod x -> Op x]
-allowdOpsForType TypeInt = return [ MkOpMod  ]
+instance Show (OpGeq e -> Op e) where
+    show _ = "(OpGeq e -> Op e)"
+
+instance ApplyOp OpGeq e where
+    applyArgs f [e1,e2] =  f (OpGeq e1 e2)
+
+
+instance OpInfo (OpOr e -> Op e) where
+    artity _             =  2
+    minDepth _           =  1
+    argsTypes _ TypeBool =  [ Ty (TyId 1) (TypeMatrix TypeInt TypeBool)]
+
+instance Show (OpOr e -> Op e) where
+    show _ = "(OpOr e -> Op e)"
+
+instance ApplyOp OpOr e where
+    applyArgs f [e1] =  f (OpOr e1)
+
+
+
+class (Monad m, Applicative m) => MonadGen m where
+    --   0    Never chosen
+    --   100  Normal chance
+    --  >100  Incressed chance
+    getOpWeighting :: (a e -> Op e) -> m Int
+
+instance MonadGen Identity where
+    getOpWeighting _ =  return 100
+
+
+-- allowdOpsForType :: MonadGen m
+--                  => Type
+--                  -> m [a x -> Op x]
+-- allowdOpsForType TypeInt = return [ MkOpMod  ]
+-- allowdOpsForType TypeBool = return [ MkOpOr, MkOpGt]
+allowdOpsForType TypeBool = return [ MkOpOr]
 
 opForType :: MonadGen m => Type -> Int -> m (Maybe Expr)
 opForType ty depth =
@@ -75,8 +105,9 @@ opForType ty depth =
 
 
 exprOfType :: Monad m => Ty -> m Expr
-exprOfType (Ty _ TypeInt) = return $ ECon (ConstantInt 1)
-
+exprOfType (Ty _ TypeInt)  = return $ ECon (ConstantInt 1)
+exprOfType (Ty _ TypeBool) = return $ ECon (ConstantBool True)
+exprOfType (Ty _ (TypeMatrix TypeInt TypeBool)) = return [essencee| [true,false] |]
 
 -- frequency3 is frequency2 that which is given the random choice
 frequency3 :: (Monad m) => Int ->  [(Int, a)] -> m a
@@ -89,3 +120,60 @@ frequency3 rnd xs0 = return $ rnd `pick` xs0
     | n <= k    = x
     | otherwise = pick (n-k) xs
   pick _ _  = error "QuickCheck2.pick used with empty list"
+
+
+-- Using names
+
+data OpName
+    = NOpGeq
+    | NOpMod
+    | NOpOr
+
+data OpInfo_ = OpInfo_
+    { artity_      :: Int
+    , minDepth_    :: Int
+    , argsTypes_   :: Map Type [Ty]
+    }   deriving (Eq, Show, Ord, Data, Typeable, Generic)
+
+op_info :: OpName -> OpInfo_
+op_info NOpGeq = OpInfo_ { artity_   = 2
+                         , minDepth_ = 1
+                         , argsTypes_ = M.fromList [
+                               (TypeBool,  [ Ty (TyId 1) TypeInt, Ty (TyId 2) TypeInt ])
+                               ]
+                         }
+
+op_info NOpOr = OpInfo_ { artity_   = 2
+                         , minDepth_ = 1
+                         , argsTypes_ = M.fromList [
+                               (TypeBool, [ Ty (TyId 1) (TypeMatrix TypeInt TypeBool)])
+                               ]
+                         }
+
+applyArgs_ :: OpName -> [e] -> (Op e)
+applyArgs_ NOpGeq [e1,e2] = MkOpGeq (OpGeq e1 e2)
+applyArgs_ NOpOr [e1]  = MkOpOr (OpOr e1)
+
+argsLookup_ :: OpName -> Type -> [Ty]
+argsLookup_ n t = fromJustNote "argsTypesL_" .  (t `M.lookup`) . argsTypes_ . op_info $ n
+
+allowdOpsForType_ TypeBool = return [ NOpGeq, NOpOr ]
+
+opForType_ :: MonadGen_ m => Type -> Int -> m (Maybe Expr)
+opForType_ ty depth =
+  filter (\p -> depth >= (minDepth_ . op_info) p ) <$> allowdOpsForType_ ty >>= \case
+   [] -> return Nothing
+   allowed -> do
+    ws      <- mapM getOpWeighting_ allowed
+    picked  <- frequency3 132 (zip ws allowed)
+
+    es <- mapM exprOfType ( argsLookup_  picked  ty)
+    let op  = applyArgs_ picked es
+    return . Just $ (EOp op)
+
+
+class (Monad m, Applicative m) => MonadGen_ m where
+    getOpWeighting_ :: OpName -> m Int
+
+instance MonadGen_ Identity where
+    getOpWeighting_ _ =  return 100
