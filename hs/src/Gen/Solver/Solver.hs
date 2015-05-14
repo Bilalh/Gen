c1801afe57a71a4b737372aad7a565b08fd70dd9
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-14 21:32:10 +0100

    More solver stuff

--- hs/src/Gen/Solver/Solver.hs
+++ hs/src/Gen/Solver/Solver.hs
@@ -9,31 +9,60 @@ import Conjure.Language.TH
 
 type Solution = Model
 
-solve :: Model -> IO Solution
+data SolveState = SolveState{
+      assigned :: [(Name,Expression)]
+    , exprs :: [Expression]
+    } deriving (Show)
+
+instance Pretty SolveState where
+    pretty = pretty . groom
+
+solve :: (MonadFail m, MonadLog m ) =>  Model -> m (Maybe Solution)
 solve model = do
-  let exprs =  concat [ xs | (SuchThat xs) <- mStatements model ]
+  let doms  =  [ (n, dom) | (Declaration (FindOrGiven Find n dom)) <- mStatements model ]
 
-  let vals = [("var1", [essence| ({} : `set of bool`) |])]
-  -- let vals = [("var1", [essence| {1} |])]
+  let start = SolveState{
+        assigned = []
+      , exprs    = concat [ xs | (SuchThat xs) <- mStatements model ]
+      }
 
-  res <- forM exprs $ \expr -> do
-                instantiateExpression vals expr
+  fin :: (Bool, SolveState) <- (flip runStateT) (start) $ do
+      end <- forM doms $ \(name,dom) -> do
+        nextVal dom >>= \case
+          Nothing -> return True
+          Just val -> do
+            modify $ \st -> st{assigned = (name,val) : assigned st }
+            return True
 
-  let sat = all isTrue res
+      return $ and end
 
-  case sat of
-    True  -> putStrLn "Vaild"
-    False -> putStrLn "Invaild"
 
-  return model
+  logInfo ("fin" <+> pretty fin )
+  -- return model
+  case fin of
+    (False, _) -> return Nothing
+    (True, SolveState{..}) -> do
+                logInfo (vcat $ map pretty $ assigned)
+                return $ Just $ createSolution $ assigned
+
+
+nextVal :: (MonadState SolveState m)
+        => Domain () Expression
+        -> m (Maybe Expression)
+nextVal _ = return $  Just [essence| ({} : `set of bool`) |]
+
 
 isTrue :: Constant -> Bool
 isTrue (ConstantBool True) = True
 isTrue _                   = False
 
+createSolution :: [(Name,Expression)] -> Solution
+createSolution xs = def{mStatements= [ Declaration $ (Letting n) e  | (n,e) <-xs ] }
+
+
 run :: IO ()
 run = do
   let fp = "/Users/bilalh/Desktop/Results/_notable/solver/a.essence"
   model <- readModelFromFile fp
-  solution <- solve model
+  solution <- runLoggerIO LogNone $ solve model
   print . pretty $ solution
