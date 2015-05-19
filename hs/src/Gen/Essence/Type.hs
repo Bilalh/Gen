e35cc766d9d1a55af2b68242c39fd19756894e94
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-19 12:19:06 +0100

    Domain types

--- hs/src/Gen/Essence/Type.hs
+++ hs/src/Gen/Essence/Type.hs
@@ -9,7 +9,7 @@ import qualified Data.Set as S
 instance Generate Type where
 
   give GOnlyLiteralTypes = give $ GOnlyTopLevel
-    [K_TypeSet, K_TypeMSet, K_TypeFunction, K_TypeRelation]
+    [K_TypeSet, K_TypeMSet, K_TypeTuple, K_TypeMatrix, K_TypeFunction]
 
   give (GOnlyTopLevel ws) = do
     defs <- gets depth >>= \d ->
@@ -17,14 +17,17 @@ instance Generate Type where
         | d == 0    -> return [ (K_TypeBool, pure TypeBool)
                               , (K_TypeInt,  pure TypeInt)
                               ]
-        | otherwise -> return [
-                         (K_TypeBool, pure TypeBool)
-                       , (K_TypeInt,  pure TypeInt)
-                       , (K_TypeSet,   liftM TypeSet   (withDepthDec (give GNone) ))
-                       -- , (K_TypeMatrix, liftM TypeMatrix (withDepthDec (give GNone) ))
-                       , (K_TypeMSet,  liftM TypeMSet  (withDepthDec (give GNone) ))
-                       -- , (K_TypePartition,   liftM TypePartition   (withDepthDec (give GNone) ))
-                       ]
+        | otherwise -> return
+            [ (K_TypeBool,     pure TypeBool)
+            , (K_TypeInt,      pure TypeInt)
+            , (K_TypeSet,      liftM TypeSet   (withDepthDec (give GNone) ))
+            , (K_TypeMSet,     liftM TypeMSet  (withDepthDec (give GNone) ))
+            , (K_TypeTuple,    liftM TypeTuple (vectorOf3 3 $ withDepthDec (give GNone)) )
+            , (K_TypeMatrix,   pure TypeMatrix   `ap` pure TypeInt
+                                                 `ap` withDepthDec (give GNone) )
+            , (K_TypeFunction, pure TypeFunction `ap` withDepthDec (give GNone)
+                                                 `ap` withDepthDec (give GNone) )
+            ]
 
     let allowed = S.fromList ws
     let ws' = [ (k,0) | k <- fieldKeys (Proxy :: Proxy Type), k `S.notMember` allowed ]
@@ -38,14 +41,17 @@ instance Generate Type where
         | d == 0    -> return [ (K_TypeBool, pure TypeBool)
                               , (K_TypeInt,  pure TypeInt)
                               ]
-        | otherwise -> return [
-                         (K_TypeBool, pure TypeBool)
-                       , (K_TypeInt,  pure TypeInt)
-                       , (K_TypeSet,   liftM TypeSet   (withDepthDec (give GNone) ))
-                       -- , (K_TypeMatrix, liftM TypeMatrix (withDepthDec (give GNone) ))
-                       , (K_TypeMSet,  liftM TypeMSet  (withDepthDec (give GNone) ))
-                       -- , (K_TypePartition,   liftM TypePartition   (withDepthDec (give GNone) ))
-                       ]
+        | otherwise -> return
+            [ (K_TypeBool,     pure TypeBool)
+            , (K_TypeInt,      pure TypeInt)
+            , (K_TypeSet,      liftM TypeSet   (withDepthDec (give GNone) ))
+            , (K_TypeMSet,     liftM TypeMSet  (withDepthDec (give GNone) ))
+            , (K_TypeTuple,    liftM TypeTuple (vectorOf3 3 $ withDepthDec (give GNone)) )
+            , (K_TypeMatrix,   pure TypeMatrix   `ap` pure TypeInt
+                                                 `ap` withDepthDec (give GNone) )
+            , (K_TypeFunction, pure TypeFunction `ap` withDepthDec (give GNone)
+                                                 `ap` withDepthDec (give GNone) )
+            ]
 
     parts <- getWeights defs
     frequency3 parts
