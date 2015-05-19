31bec851db3047bf6479a4221283d7f59cd1d264
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-19 16:13:23 +0100

    literals

--- hs/src/Gen/Essence/Literal.hs
+++ hs/src/Gen/Essence/Literal.hs
@@ -32,11 +32,43 @@ instance (Generate a, WrapConstant a) => Generate (AbstractLiteral a, Type) wher
     es <- vectorOf3 num (withDepthDec $ give (GType ty))
     return $ (AbsLitSet es, r)
 
+  give (GType r@(TypeMSet ty)) = do
+    num <- choose3 (0,5)
+    es <- vectorOf3 num (withDepthDec $ give (GType ty))
+    return $ (AbsLitMSet es, r)
+
   give (GType r@(TypeMatrix TypeInt ty)) = do
     numElems <- choose3 (0,5)
     es <- vectorOf3 numElems (withDepthDec $ give (GType ty))
     idx <- intDomainOfSize (fromIntegral numElems)
     return $ (AbsLitMatrix idx es, r)
+
+  give (GType r@(TypeFunction t1 t2)) = do
+    numElems <- choose3 (0,5)
+    e1 <- vectorOf3 numElems (withDepthDec $ give (GType t1))
+    e2 <- vectorOf3 numElems (withDepthDec $ give (GType t2))
+    let es = zip e1 e2
+    return $ (AbsLitFunction es, r)
+
+  give (GType r@(TypeRelation ts)) = do
+    es <- forM ts $ \t -> do
+            numElems <- choose3 (0,5)
+            vectorOf3 numElems (withDepthDec $ give (GType t))
+    return $ (AbsLitRelation es, r)
+
+  give (GType r@(TypePartition t)) = do
+    n  <- choose3 (0,5)
+    es <- replicateM n $ do
+            numElems <- choose3 (0,5)
+            vectorOf3 numElems (withDepthDec $ give (GType t))
+
+    return $ (AbsLitPartition es, r)
+
+  give (GType r@(TypeTuple ts)) = do
+    es <- forM ts $ \t -> do
+            withDepthDec $ give (GType t)
+
+    return $ (AbsLitTuple es, r)
 
   give t = giveUnmatched "Generate (AbstractLiteral a)" t
 
