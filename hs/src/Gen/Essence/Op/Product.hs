557087a5e27c59000dd9af49a0a14ab84d3f0813
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-20 04:14:47 +0100

    ops

---
+++ hs/src/Gen/Essence/Op/Product.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Gen.Essence.Op.Product where
+
+import Conjure.Language.AdHoc
+import Conjure.Language.Expression.Op
+import Gen.Essence.St
+import Gen.Essence.Type               ()
+import Gen.Imports
+
+
+instance (Generate a, ExpressionLike a) => Generate (OpProduct a) where
+  give GNone = give (GType TypeInt)
+
+  give (GType TypeInt) = OpProduct <$> give  (GType $ TypeMatrix TypeInt TypeInt)
+
+  give t = giveUnmatched "Generate OpProduct" t
+
+  possiblePure _ (Just ty) _ | ty /= TypeInt = False
+  possiblePure _ _ d = d >= 2
+
+  requires _ _ = [RAll [K_TypeInt]]
