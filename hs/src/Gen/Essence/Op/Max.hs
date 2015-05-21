64b014ea4e74a28d352684462f54e6ca35bebbe7
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-21 04:14:51 +0100

    better max

--- hs/src/Gen/Essence/Op/Max.hs
+++ hs/src/Gen/Essence/Op/Max.hs
@@ -26,20 +26,19 @@ instance (Generate a, ExpressionLike a, EvalToInt a, WrapConstant a)
     d <- gets depth
     defs <- getPossibilities d [
         -- (return . (>=1), (K_DomainInt,  wrapDomain <$> dgive ty) )
-        (return . (>=2), (K_TypeSet,    give (GType $ TypeSet  TypeInt))  )
-      , (return . (>=2), (K_TypeMSet,   give (GType $ TypeMSet TypeInt))  )
-      , (return . (>=2), (K_TypeMatrix, give (GType $ TypeMatrix TypeInt TypeInt))  )
+        (return . (>=1), (K_TypeSet,    give (GType $ TypeSet  TypeInt))  )
+      , (return . (>=1), (K_TypeMSet,   give (GType $ TypeMSet TypeInt))  )
+      , (return . (>=1), (K_TypeMatrix, give (GType $ TypeMatrix TypeInt TypeInt))  )
       ]
-    when (all ( (<=0) . fst) defs ) $ nnError $line $ map (pretty . fst) defs
+    freqError $line defs
     picked <- frequency3 defs
     return $ OpMax picked
 
   give t = giveUnmatched "Generate OpMax" t
 
   -- possiblePure _ (Just ty) d | ty `elem` [TypeBool, TypeInt] = d >= 1
-  possiblePure _ (Just ty) d | ty `elem` [TypeInt] = d >= 2
-  possiblePure _ (Just _) _ = False
-  possiblePure _ Nothing _  = False
+  possiblePure _ (Just ty) d | ty `elem` [TypeInt] = d >= 1
+  possiblePure _ _ _  = False
 
   -- requires _ (Just TypeBool) = [RAll [K_DomainBool] ]
   requires _ (Just TypeInt) =
