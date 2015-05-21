01df5ca56933915b453b5f1095c820317f582454
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-21 03:13:53 +0100

    using nicer error func

--- hs/src/Gen/Essence/Rnd.hs
+++ hs/src/Gen/Essence/Rnd.hs
@@ -8,14 +8,14 @@ import Test.QuickCheck             (choose,elements,choose)
 
 frequency3 :: [(Int, GenSt a)] -> GenSt a
 frequency3 [] = error "frequency3 used with empty list"
-frequency3 xs0 =  choose3 (1, tot) >>= (`pick` xs0)
+frequency3 xs0 = choose3 (1, tot) >>= (`pick` xs0)
  where
   tot = sum (map fst xs0)
 
   pick n ((k,x):xs)
     | n <= k    = x
     | otherwise = pick (n-k) xs
-  pick _ _  = error "frequency3.pick used with empty list"
+  pick _ _  = nnError "frequency3.pick used with empty list" []
 
 
 elemFreq3 :: [(Int, a)] -> GenSt a
@@ -27,14 +27,14 @@ elemFreq3 xs0 =  choose3 (1, tot) >>= (`pick` (map (\(a,b) -> (a,pure b) ) xs0))
   pick n ((k,x):xs)
     | n <= k    = x
     | otherwise = pick (n-k) xs
-  pick _ _  = error "frequency3.pick used with empty list"
+  pick _ _  = nnError "frequency3.pick used with empty list" []
 
 
 choose3 :: Random a => (a,a) -> GenSt a
 choose3 rng = lift . lift $ choose rng
 
 oneof3 :: [GenSt a] -> GenSt a
-oneof3 [] = error "oneof3 used with empty list"
+oneof3 [] = nnError "oneof3 used with empty list" []
 oneof3 gs = choose3 (0,length gs - 1) >>=   (gs `at`)
 
 elements3 :: [a] -> GenSt a
