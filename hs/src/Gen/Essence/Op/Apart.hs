92e1842943eaad48e011192e3ed01735e3d35671
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-21 05:03:18 +0100

    apart

---
+++ hs/src/Gen/Essence/Op/Apart.hs
@@ -0,0 +1,22 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Gen.Essence.Op.Apart (Generate(..)) where
+
+import Conjure.Language.AdHoc
+import Conjure.Language.Expression.Op
+import Gen.Essence.St
+import Gen.Essence.Type               ()
+import Gen.Imports
+
+instance (Generate a, ExpressionLike a) => Generate (OpApart a) where
+  give GNone = give (GType TypeBool)
+
+  give (GType TypeBool) = do
+      ty <- dgive GNone
+      OpApart <$> give (GType $ TypeSet ty) <*> give (GType $ TypePartition ty)
+
+  give t = giveUnmatched "Generate OpApart" t
+
+  possiblePure _ (Just ty ) _ | ty /= TypeBool = False
+  possiblePure _ _ d = d>1
+
+  requires _ _ = [RAll $ [K_TypeSet, K_TypePartition]]
