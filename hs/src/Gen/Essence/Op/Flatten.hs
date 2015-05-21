cd8cc3fb1f7819272b95315479a736237f0ecf0a
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-21 17:38:11 +0100

    flatten

---
+++ hs/src/Gen/Essence/Op/Flatten.hs
@@ -0,0 +1,32 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Gen.Essence.Op.Flatten (Generate(..)) where
+
+import Conjure.Language.AdHoc
+import Conjure.Language.Expression.Op
+import Gen.Essence.Id
+import Gen.Essence.Rnd
+import Gen.Essence.St
+import Gen.Essence.Type               ()
+import Gen.Imports
+
+instance (Generate a, ExpressionLike a) => Generate (OpFlatten a) where
+  give GNone = do
+    ty <- withWeights [(K_TypeMatrix, 0)] $ give GNone
+    give (GType $ TypeMatrix TypeInt ty)
+
+  give (GType (TypeMatrix TypeInt inn )) | inn /= TypeMatrix{}  = do
+    d <- gets depth
+    n <- choose3 (1,d-1)
+
+    let wrap = foldr1 (flip (.))  $ replicate n (TypeMatrix TypeInt)
+    OpFlatten <$> give (GType $ wrap inn)
+
+  give t = giveUnmatched "Generate OpFlatten" t
+
+  possiblePure _ (Just (TypeMatrix TypeInt inn )) d | inn /= TypeMatrix{}  = d >= 1
+
+  possiblePure _ (Just _ ) _ = False
+  possiblePure _ Nothing d   = d >=1
+
+  requires _ (Just ty) = [RAll $ keyList ty]
+  requires _ _         = [RAll [K_TypeMatrix] , RAny [K_TypeInt] ]
