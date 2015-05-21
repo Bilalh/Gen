6a5385dfd39801745f380c43b769345a013d004c
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-21 18:10:11 +0100

    freq

---
+++ hs/src/Gen/Essence/Op/Freq.hs
@@ -0,0 +1,24 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Gen.Essence.Op.Freq (Generate(..)) where
+
+import Conjure.Language.AdHoc
+import Conjure.Language.Expression.Op
+import Gen.Essence.St
+import Gen.Essence.Type               ()
+import Gen.Imports
+
+instance (Generate a, ExpressionLike a) => Generate (OpFreq a) where
+  give GNone = give (GType TypeInt)
+
+  give (GType TypeInt) = do
+    m@(TypeMSet x) <- give (GOnlyTopLevel [K_TypeMSet])
+    OpFreq <$> give (GType m) <*> give (GType x)
+
+  give t = giveUnmatched "Generate OpFreq" t
+
+  possiblePure _ (Just TypeInt) d = d >=1
+
+  possiblePure _ (Just _ ) _ = False
+  possiblePure _ Nothing d   = d >= 1
+
+  requires _ _ = [RAny [K_TypeInt, K_TypeBool], RAll [K_TypeSet] ]
