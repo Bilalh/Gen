aa5842cd958968e4ab1c6eca6ebb47eec0d58691
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-21 03:14:43 +0100

    type classes

--- hs/src/Gen/Essence/Op.hs
+++ hs/src/Gen/Essence/Op.hs
@@ -1,15 +1,17 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Gen.Essence.Op (module X) where
 
+import Conjure.Language.AdHoc
 import Conjure.Language.Expression.Op
+import Gen.Essence.EvalToInt
 import Gen.Essence.Op.Internal.Generated as X
 import Gen.Essence.Rnd
 import Gen.Essence.St
 import Gen.Helpers.SizeOf
 import Gen.Imports
-import Conjure.Language.AdHoc
 
-instance (Generate a, ExpressionLike a) => Generate (Op a) where
+instance (Generate a, ExpressionLike a, EvalToInt a, WrapConstant a)
+    => Generate (Op a) where
   give a = do
       ops <- getPossibilities a (allOps a)
       withDepthDec $ frequency3 ops
