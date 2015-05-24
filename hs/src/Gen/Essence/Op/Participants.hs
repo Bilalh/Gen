5813cca38b171049f46871ffc502f2063b2dce5e
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-24 16:48:31 +0100

    misc

--- hs/src/Gen/Essence/Op/Participants.hs
+++ hs/src/Gen/Essence/Op/Participants.hs
@@ -17,8 +17,8 @@ instance (Generate a, ExpressionLike a) => Generate (OpParticipants a) where
   give t = giveUnmatched "Generate OpParticipants" t
 
   possiblePure _ (Just (TypeSet ty)) d = fromIntegral d >= depthOf ty + 1
-  possiblePure _ Just{} _    = False
-  possiblePure _ Nothing _   = False
+  possiblePure _ Just{} _              = False
+  possiblePure _ Nothing _             = False
 
   requires _ (Just ty@TypeSet{}) = [RAll $ keyList ty, RAll [K_TypePartition]]
   requires _ _         = []
