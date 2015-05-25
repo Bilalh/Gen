75859558866dd828f38279c4df46aee8ec77e3d3
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-25 02:57:35 +0100

    minus

---
+++ hs/src/Gen/Essence/Op/Minus.hs
@@ -0,0 +1,31 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+module Gen.Essence.Op.Minus (Generate(..)) where
+
+import Conjure.Language.AdHoc
+import Conjure.Language.Expression.Op
+import Gen.Essence.Id
+import Gen.Essence.St
+import Gen.Essence.Type               ()
+import Gen.Helpers.SizeOf
+import Gen.Imports
+
+
+instance (Generate a, ExpressionLike a) => Generate (OpMinus a) where
+  give ty@GType{} = pure OpMinus <*> give ty <*> give ty
+  give t          = giveUnmatched "Generate OpMinus" t
+
+  possiblePure _ (Just ty) d = allow ty && fromIntegral d >= depthOf ty
+    where
+      allow TypeInt{}       = True
+      allow TypeSet{}       = True
+      allow TypeMSet{}      = True
+      allow TypeFunction{}  = True
+      allow TypeRelation{}  = True
+      allow TypePartition{} = True
+      allow _               = False
+
+
+  possiblePure _ Nothing _   = False
+
+  requires _ (Just ty) = [RAll $ keyList ty]
+  requires _ _         = []
