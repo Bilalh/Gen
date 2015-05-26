d783745ab09f5f280d381b1d588fba51bd8cb890
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-05-26 22:43:42 +0100

    new test

--- hs/src/test/Gen/SimplerTest.hs
+++ hs/src/test/Gen/SimplerTest.hs
@@ -58,6 +58,12 @@ tests = testGroup "simpler"
        \(AType a) ->  (runIdentity $ simpler a a) == EQ
    , no $ QC.testProperty "atype and depthOf argee" $
        \(BType ty gen_depth) ->  depthOf ty == fromIntegral gen_depth
+   , Just $ QC.testProperty "simpler is consistent" $
+       \(SType a) (SType b) -> do
+         let simpler_ab = runIdentity $ simpler a b
+         let simpler_ba = runIdentity $ simpler b a
+         simpler_ab    == negOrder simpler_ba
+
    ]
 
   ,testGroup "Expr_gen eq"
@@ -130,7 +136,6 @@ newtype AType =  AType Type
 instance Arbitrary AType where
     arbitrary = flip evalStateT def{depth_=3} $ fmap AType atype
 
-
 data BType =  BType Type Int
     deriving (Show,Eq)
 
@@ -139,3 +144,11 @@ instance Arbitrary BType where
       i <- choose (1,  (max 0 (min s 3)) )
       res <- flip evalStateT def{depth_=i} atype
       return $ BType res i
+
+data SType =  SType Type
+    deriving (Show,Eq)
+
+instance Arbitrary SType where
+  arbitrary = sized $ \s -> do
+    i <- choose (1,  (max 0 (min s 3)) )
+    SType <$> flip evalStateT def{depth_=i} atype
