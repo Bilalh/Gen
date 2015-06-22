856b4c7476fa0a8d109015d3936bbae35a4d7252
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-06-22 11:54:46 +0100

    0 weight stays at zero

--- hs/src/Gen/Essence/Adjust.hs
+++ hs/src/Gen/Essence/Adjust.hs
@@ -28,6 +28,7 @@ doWeighting m k =
       True  ->  M.adjust decreaseWeighting k m
       False ->  M.insert k 95 m
 
--- | decrease 1the weighting by 5%, min 1,  n.b. scale is [0,100+]
+-- | decrease the weighting by 5%, min 1,  n.b. scale is [0,100+]
 decreaseWeighting :: Int -> Int
+decreaseWeighting 0 = 0
 decreaseWeighting a = max 1 (truncate $ fromIntegral a * (0.95 :: Double))
