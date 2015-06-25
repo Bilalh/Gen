9efe93613db80afc853123bf9baa58e9c9211c81
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-06-25 23:47:20 +0100

    reformat using yapf

--- toolchain/toolchain.py
+++ toolchain/toolchain.py
@@ -25,13 +25,13 @@ from toolchain import run
 from toolchain.command import K
 from toolchain.run import Status
 
-
 logger = logging.getLogger(__name__)
 
 
 def sha1_file(filepath):
     with filepath.open('rb') as f:
         return hashlib.sha1(f.read()).hexdigest()
+
 
 def obj_to_json(obj):
     if isinstance(obj, Enum):
@@ -43,19 +43,18 @@ def obj_to_json(obj):
 
 def with_settings(results, *, op, time_taken, successful, consistent):
     return dict(
-        data_       = results,
-        essence_    = op.essence,
-        outdir_     = op.outdir,
-        given_time_ = op.timeout,
-        time_taken_ = time_taken,
-        successful_ = successful,
-        consistent_ = consistent
-    )
+        data_=results,
+        essence_=op.essence,
+        outdir_=op.outdir,
+        given_time_=op.timeout,
+        time_taken_=time_taken,
+        successful_=successful,
+        consistent_=consistent)
 
 
 def remove_solve_dups(solve_results, outdir):
-    exts = ['.eprime.logs', '.eprime.error', '.eprimes-solution', '.solution',
-             '.output', '.eprime-param', '.refine-output', '.eprime', '.choices.json']
+    exts = ['.eprime.logs', '.eprime.error', '.eprimes-solution', '.solution', '.output',
+            '.eprime-param', '.refine-output', '.eprime', '.choices.json']
 
     error_hashes = defaultdict(set)
 
@@ -63,9 +62,9 @@ def remove_solve_dups(solve_results, outdir):
         logger.warn("Removing %s it has the same error as %s ", ep.stem, err_hash)
         for ext in exts:
             if ep.with_suffix(ext).exists():
-                    ep.with_suffix(ext).unlink()
+                ep.with_suffix(ext).unlink()
 
-    results_unique=[]
+    results_unique = []
 
     for (eprime_name, dic) in solve_results.items():
         if dic['last_kind'] in [K.savilerow, K.translateUp, K.validate]:
@@ -73,7 +72,7 @@ def remove_solve_dups(solve_results, outdir):
                 delete((outdir / eprime_name), "<passing>")
                 continue
             solve_output = (outdir / eprime_name).with_suffix(".output")
-            err_hash = run.hash_path( solve_output )
+            err_hash = run.hash_path(solve_output)
             if err_hash in error_hashes[dic['last_kind']]:
                 delete((outdir / eprime_name), err_hash)
                 continue
@@ -84,6 +83,7 @@ def remove_solve_dups(solve_results, outdir):
         results_unique.append((eprime_name, dic))
 
     return dict(results_unique)
+
 
 if __name__ == "__main__":
 
@@ -97,13 +97,15 @@ if __name__ == "__main__":
 
         consoleHandler = logging.StreamHandler()
         consoleHandler.setLevel(logging.WARNING)
-        logFormatter = logging.Formatter('%(name)s:%(lineno)d:%(funcName)s:\n    %(message)s')
+        logFormatter = logging.Formatter(
+            '%(name)s:%(lineno)d:%(funcName)s:\n    %(message)s')
         consoleHandler.setFormatter(logFormatter)
         logging.getLogger().addHandler(consoleHandler)
 
         fileHandler = logging.FileHandler(str(p))
         fileHandler.setLevel(logging.INFO)
-        logFormatter = logging.Formatter('%(asctime)s⦙%(levelname)-10s⦙%(name)-10s⦙%(lineno)-4d⦙%(funcName)-50s⦙ %(message)s')
+        logFormatter = logging.Formatter(
+            '%(asctime)s⦙%(levelname)-10s⦙%(name)-10s⦙%(lineno)-4d⦙%(funcName)-50s⦙ %(message)s')
         fileHandler.setFormatter(logFormatter)
         logging.getLogger().addHandler(fileHandler)
 
@@ -112,26 +114,21 @@ if __name__ == "__main__":
     logger.warn("args %s", op)
     logger.info("@@sha1_spec.essence:%s", sha1_file(op.essence))
 
-    here_dir=Path(__file__).parent
-
+    here_dir = Path(__file__).parent
 
     if op.bin_dir:
-        extra_env = dict(PATH= str(here_dir) + ":" +  op.bin_dir + ":" + os.environ['PATH'])
+        extra_env = dict(PATH=str(here_dir) + ":" + op.bin_dir + ":" + os.environ['PATH'])
     else:
-        extra_env = dict(PATH= str(here_dir) + ":" + os.environ['PATH'])
+        extra_env = dict(PATH=str(here_dir) + ":" + os.environ['PATH'])
 
     if 'LD_LIBRARY_PATH' in os.environ:
         logging.warn("LD_LIBRARY_PATH=%s", os.environ['LD_LIBRARY_PATH'])
     else:
         logging.warn("LD_LIBRARY_PATH unset")
 
-
     logger.warn("new PATH=%s", extra_env['PATH'])
 
-
-
     random.seed(op.seed)
-
 
     if op.new_conjure:
         if not which('conjureNew'):
@@ -142,54 +139,55 @@ if __name__ == "__main__":
     else:
         commands = command.ConjureOld()
 
-
     def get_refine_time(fp, arg):
         with (fp).open() as f:
             data = json.load(f)['data_']
             if "choices_made" in data:
                 return data['cmd_used']['cpu_time']
             else:
-                refine_times = { k:vs['cpu_time']  for (k,vs) in data.items()   }
-                if arg  not in refine_times:
+                refine_times = {k: vs['cpu_time'] for (k, vs) in data.items()}
+                if arg not in refine_times:
                     logger.error('{} not in {}'.format(arg, fp))
                     sys.exit(9)
                 return refine_times[arg]
 
     def get_solve_time(fp, arg):
         with (fp).open() as f:
-            solve_times = { k:vs['total_cpu_time']  for (k,vs) in  json.load(f)['data_'].items()   }
+            solve_times = {
+                k: vs['total_cpu_time']
+                for (k, vs) in json.load(f)['data_'].items()
+            }
             if arg not in solve_times:
                 if arg == 'follow':
-                   return max(solve_times.values())
+                    return max(solve_times.values())
                 else:
                     logger.error('%s not in %s', arg, fp)
                     sys.exit(10)
             return solve_times[arg]
 
-
     if op.reduce_time:
         refine = op.essence.with_name('refine_essence.json')
-        solve  = op.essence.with_name('solve_eprime.json')
+        solve = op.essence.with_name('solve_eprime.json')
 
-        new_timeout=None
+        new_timeout = None
         if solve.exists():
             if not refine.exists():
                 logger.error("ERROR: solve_eprime exists but refine_essence is missing")
                 sys.exit(8)
             else:
                 refine_time = get_refine_time(refine, op.reduce_time)
-                solve_time  = get_solve_time(solve, op.reduce_time)
+                solve_time = get_solve_time(solve, op.reduce_time)
                 new_timeout = refine_time + solve_time
-
 
         elif refine.exists():
             new_timeout = get_refine_time(refine, op.reduce_time)
 
         if new_timeout:
             if new_timeout > op.timeout:
-                logger.warn("Timeout previously used %s, is less then given time %s", new_timeout, op.timeout)
-            old=op.timeout
-            op.timeout = int(min( max(new_timeout * 3, 20), op.timeout))
+                logger.warn("Timeout previously used %s, is less then given time %s",
+                            new_timeout, op.timeout)
+            old = op.timeout
+            op.timeout = int(min(max(new_timeout * 3, 20), op.timeout))
 
             logger.warn("Timeout changed from %s to %s", old, op.timeout)
 
@@ -202,36 +200,44 @@ if __name__ == "__main__":
             sys.exit(4)
 
         (essence_refine, refine_wall_time) = run.run_refine_essence_with_choices(
-                op=op, commands=commands, extra_env=extra_env)
-        successful = essence_refine['cmd_used']['status_'] in [Status.success, Status.timeout]
+            op=op,
+            commands=commands,
+            extra_env=extra_env)
+        successful = essence_refine['cmd_used']['status_'] in [Status.success,
+                                                               Status.timeout]
     elif op.refine_all:
         (essence_refine, refine_wall_time) = run.run_refine_all_essence(
-            op=op, commands=commands, extra_env=extra_env)
-        successful = essence_refine['cmd_used']['status_'] in [Status.success, Status.timeout]
+            op=op,
+            commands=commands,
+            extra_env=extra_env)
+        successful = essence_refine['cmd_used']['status_'] in [Status.success,
+                                                               Status.timeout]
     else:
         (essence_refine, refine_wall_time) = run.run_refine_essence(
-            op=op, commands=commands, random=op.num_cores - 1, cores=op.num_cores,
+            op=op,
+            commands=commands,
+            random=op.num_cores - 1,
+            cores=op.num_cores,
             extra_env=extra_env)
-        successful = all(  res['status_'] in [Status.success, Status.timeout]
-            for res in essence_refine.values() )
+        successful = all(res['status_'] in [Status.success, Status.timeout]
+                         for res in essence_refine.values())
 
     endTime = time.time()
     logger.info("essence_refine: %s", pformat(essence_refine))
 
-
-    settings = with_settings(essence_refine, op=op,
-            time_taken=refine_wall_time,
-            successful=successful, consistent=True)
-    with (op.outdir / "refine_essence.json" ).open("w") as f:
-        f.write(json.dumps(settings, indent=True, sort_keys=True, default=obj_to_json ))
-
+    settings = with_settings(essence_refine,
+                             op=op,
+                             time_taken=refine_wall_time,
+                             successful=successful,
+                             consistent=True)
+    with (op.outdir / "refine_essence.json").open("w") as f:
+        f.write(json.dumps(settings, indent=True, sort_keys=True, default=obj_to_json))
 
     if not successful:
         sys.exit(3)
 
     if op.refine_only:
         sys.exit(0)
-
 
     eprimes = list(op.outdir.glob('*.eprime'))
 
@@ -245,9 +251,9 @@ if __name__ == "__main__":
     # limit = op.timeout - refine_wall_time
 
     # use clock time
-    limit = op.timeout - (endTime - startTime )
+    limit = op.timeout - (endTime - startTime)
 
-    if limit <=0:
+    if limit <= 0:
         logger.warn("No time left after refine")
         sys.exit(0)
 
@@ -258,9 +264,9 @@ if __name__ == "__main__":
     solve_results = dict(pool.map(solve_op, eprimes))
     logger.info("solve_results: %s", pformat(solve_results))
 
-    solve_wall_time = sum(  res['total_real_time'] for res in solve_results.values()  )
+    solve_wall_time = sum(res['total_real_time'] for res in solve_results.values())
 
-    successful = all( res['erroed'] is None for res in solve_results.values() )
+    successful = all(res['erroed'] is None for res in solve_results.values())
 
     if not successful:
         solve_results = remove_solve_dups(solve_results, op.outdir)
@@ -268,18 +274,20 @@ if __name__ == "__main__":
     # checks that all eprimes that were solved either have a solution
     # or don't have a solution
     def is_consistent():
-        fin_names = [ k for (k, v) in solve_results.items()
-                        if not v['erroed'] and v['solving_finished'] ]
-        sol_exist = [ (op.outdir / name).with_suffix(".solution").exists()
-                        for name in fin_names ]
-        return all(sol_exist) or all( [ not b for b in sol_exist])
+        fin_names = [k for (k, v) in solve_results.items()
+                     if not v['erroed'] and v['solving_finished']]
+        sol_exist = [(op.outdir / name).with_suffix(".solution").exists()
+                     for name in fin_names]
+        return all(sol_exist) or all([not b for b in sol_exist])
 
-    settings = with_settings(solve_results, op=op,
-            time_taken=solve_wall_time, successful=successful, consistent=is_consistent())
+    settings = with_settings(solve_results,
+                             op=op,
+                             time_taken=solve_wall_time,
+                             successful=successful,
+                             consistent=is_consistent())
 
-    with (op.outdir / "solve_eprime.json" ).open("w") as f:
-        f.write(json.dumps(settings,
-            indent=True, sort_keys=True, default=obj_to_json ))
+    with (op.outdir / "solve_eprime.json").open("w") as f:
+        f.write(json.dumps(settings, indent=True, sort_keys=True, default=obj_to_json))
 
 # logger.info("\033[1;31mtotal_cpu_time:%0.2f  total_real_time:%0.2f\033[1;0m",
 #         total_cpu_time, total_real_time)
@@ -289,7 +297,3 @@ if __name__ == "__main__":
 
     logger.warn("completed")
     sys.exit(0)
-
-
-
-
