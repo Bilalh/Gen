3c172ff445ce82e4d8d75ead20da78b3399eeb3c
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-06-28 02:07:06 +0100

    misc

--- hs/src/Gen/Essence/Op/AllDiff.hs
+++ hs/src/Gen/Essence/Op/AllDiff.hs
@@ -7,8 +7,6 @@ import Gen.Essence.Type               ()
 import Gen.Imports
 
 instance Generate a => Generate (OpAllDiff a) where
-  give GNone = give (GType TypeBool)
-
   give (GType TypeBool) = do
       ty <- give (GOnlyTopLevel [K_TypeMatrix] )
       pure OpAllDiff <*> give (GType ty)
@@ -16,7 +14,6 @@ instance Generate a => Generate (OpAllDiff a) where
   give t = giveUnmatched "Generate OpAllDiff" t
 
   possiblePure _ (Just TypeBool ) d = d >= 1
-  possiblePure _ Just{} _           = False
-  possiblePure _ _ d                = d >= 1
+  possiblePure _ _ _                = False
 
   requires _ _ = [RAll $ [K_TypeMatrix]]
