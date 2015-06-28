8990299c77060015683b206baeaaf78b60960e01
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-06-28 04:59:24 +0100

    log

--- hs/src/Gen/Essence/Log.hs
+++ hs/src/Gen/Essence/Log.hs
@@ -1,17 +1,8 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Gen.Essence.Log where
 import Gen.Imports
-
--- import System.IO.Unsafe(unsafePerformIO)
--- import System.IO(appendFile)
--- instance (Monad a, Applicative a)
---       => MonadLog (WriterT [(LogLevel, Doc)] a)  where
---     log lvl msg = do
---       unsafePerformIO $ do
---          appendFile "_unsafe.log" (show msg)
---          return $ tell []
---       tell [(lvl, msg)]
 
 instance (Monad a, Applicative a)
       => MonadLog (WriterT [(LogLevel, Doc)] a)  where
@@ -20,3 +11,9 @@ instance (Monad a, Applicative a)
       when (lvl <= LogInfo) $  trace (" ^" ++ show lvl ++ " " ++ show msg)
 #endif
       tell [(lvl, msg)]
+
+newtype LogT m a = LogT (WriterT [(LogLevel, Doc)] m a)
+    deriving (Functor, Applicative, Monad, MonadTrans, MonadIO)
+
+instance (Applicative m, Monad m) => MonadLog (LogT m) where
+    log lvl msg = LogT $ tell [(lvl, msg)]
