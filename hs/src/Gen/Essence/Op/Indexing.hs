fb80a247dd479cb06d7e89ca1c77c7851106f8b4
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-07-06 15:59:26 +0100

    changed tuple indexing

--- hs/src/Gen/Essence/Op/Indexing.hs
+++ hs/src/Gen/Essence/Op/Indexing.hs
@@ -2,6 +2,7 @@
 module Gen.Essence.Op.Indexing (Generate(..)) where
 
 import Conjure.Language.AdHoc
+import Conjure.Language.Constant
 import Conjure.Language.Expression.Op
 import Gen.Essence.Id
 import Gen.Essence.Ints
@@ -26,24 +27,8 @@ instance (Generate a, ExpressionLike a, GenInfo a, EvalToInt a)
          logInfo2 $line ["K_TypeMatrix", nn "depth" d,  nn "inn" inn]
          -- TODO in bounds
          OpIndexing <$> give (GType $ TypeMatrix TypeInt inn) <*> give (GType TypeInt)
-       K_TypeTuple -> do
-         n <- choose3 (1,4)
-         (IntRanged c ci) <- give (GIntRanged 1 n)
-         logInfo2 $line ["K_TypeTuple"
-                        , nn "depth" d
-                        , nn "inn" inn
-                        , nn "n" n
-                        , nn "c" c]
-         vs :: [Type] <- forM [1..n] $ \i -> do
-                         if i == ci then
-                             pure inn
-                         else
-                             dgive GNone
-         logInfo2 $line $ "K_TypeTuple" : map pretty vs
 
-
-         OpIndexing <$> give (GType $ TypeTuple vs)
-                    <*> pure c
+       K_TypeTuple -> tupleIndexing d inn
 
        _ -> $never
 
@@ -54,3 +39,51 @@ instance (Generate a, ExpressionLike a, GenInfo a, EvalToInt a)
 
   requires _ (Just ty) = [RAll $ keyList ty, RAny [K_TypeMatrix, K_TypeMatrix]]
   requires _ _         = []
+
+
+tupleIndexing :: forall b a. (EvalToInt a, Generate a, GenInfo a, Pretty b)
+              => b
+              -> Type
+              -> GenSt (OpIndexing a)
+tupleIndexing d inn = do
+  n  :: Int    <- choose3 (1,4)
+  c  :: Int    <- choose3 (1,n)
+  logInfo2 $line ["K_TypeTuple"
+                , nn "depth" d
+                , nn "inn" inn
+                , nn "n" n
+                , nn "c" c]
+  vs :: [Type] <- forM [1..n] $ \i -> do
+                 if i == c then
+                     pure inn
+                 else
+                     dgive GNone
+  logInfo2 $line $ "K_TypeTuple" : map pretty vs
+
+  OpIndexing <$> give (GType $ TypeTuple vs)
+                  <*> pure (wrapConstant $ ConstantInt (fromIntegral c))
+
+
+-- | Generate A tuple indexing expresion using constant int expressions
+_oldTupleIndexing :: forall b a. (EvalToInt a, Generate a, GenInfo a, Pretty b)
+                  => b
+                  -> Type
+                  -> GenSt (OpIndexing a)
+_oldTupleIndexing d inn= do
+  n <- choose3 (1,4)
+  (IntRanged c ci) <- give (GIntRanged 1 n)
+  logInfo2 $line ["K_TypeTuple"
+                , nn "depth" d
+                , nn "inn" inn
+                , nn "n" n
+                , nn "c" c]
+  vs :: [Type] <- forM [1..n] $ \i -> do
+                 if i == ci then
+                     pure inn
+                 else
+                     dgive GNone
+  logInfo2 $line $ "K_TypeTuple" : map pretty vs
+
+
+  OpIndexing <$> give (GType $ TypeTuple vs)
+            <*> pure c
