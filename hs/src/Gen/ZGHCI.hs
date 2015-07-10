aa05c390ce5fc03246bb48001494b121ebdfca5b
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-07-10 16:34:47 +0100

    misc

--- hs/src/Gen/ZGHCI.hs
+++ hs/src/Gen/ZGHCI.hs
@@ -34,8 +34,10 @@ import Gen.Helpers.SizeOf             as X
 import Gen.Helpers.TypeOf             as X
 import Gen.Imports                    as X
 import Gen.IO.Formats                 as X
+import Gen.IO.ToolchainData           as X
 import Gen.Reduce.Reduction           as X
 import Gen.Reduce.Simpler             as X
+
 
 import qualified Data.Map as M
 
