435dbf78f4a8a21691ae4f8c838306c12c4dabdd
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-07-19 20:49:49 +0100

    script to install specific version now work with meta.sqlite

--- toolchain/binaries_toolchain_install_line.py
+++ toolchain/binaries_toolchain_install_line.py
@@ -4,22 +4,28 @@ import argparse
 import logging
 import sqlite3
 import sys
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 parser = argparse.ArgumentParser()
 
-parser.add_argument("db", help='info.sqlite')
-parser.add_argument("num", help='group id', type=int)
+parser.add_argument("db", help='meta.sqlite/info.sqlite')
+parser.add_argument("num", help='collection/group id', type=int)
 args = parser.parse_args()
 
-get_query = "SELECT * FROM ToolchainGroup WHERE groupId = ?;"
+fmtstring = "BUILD=yes SR_REV={savilerow} MINION_REV={minion} CONJURE_REV={conjureNew} ~/server-settings/install_repos.sh"
+
+if Path(args.db).name == "info.sqlite":
+    get_query = "SELECT * FROM ToolchainGroup WHERE groupId = ?;"
+else:
+    get_query = "SELECT * FROM ToolchainCollection WHERE collectionId = ?;"
+
 with sqlite3.connect(args.db) as conn:
     conn.row_factory = sqlite3.Row
-    res = list(conn.execute(get_query, (args.num,) ))
+    res = list(conn.execute(get_query, (args.num, )))
     if not res:
-        print("Group does not exist : {} ".format(args.num))
+        print("Group/Collection does not exist : {} ".format(args.num))
         sys.exit(1)
     else:
         row = res[0]
-        print("SR_REV={savilerow} MINION_REV={minion} CONJURE_REV={conjureNew} ~/server-settings/install_repos.sh ".format(**row))
-
+        print(fmtstring.format(**row))
