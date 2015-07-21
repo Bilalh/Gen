fa419e96ce199349917ae492f31f90381c186cf1
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-07-21 01:30:08 +0100

    reduction script

--- toolchain/eprime_to_choices.py
+++ toolchain/eprime_to_choices.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
-import argparse
 import json
 import logging
 
@@ -8,45 +7,52 @@ from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
-parser = argparse.ArgumentParser()
-parser.add_argument("eprime")
-parser.add_argument('--update',  action='store_true',help='Update old choices')
-args = parser.parse_args()
-
-eprime = Path(args.eprime)
-with eprime.open("r") as f:
-    json_lines=[  line[1:] for line in f  if line[0]=='$' ]
-
-data=json.loads(" ".join(json_lines[1:]))
-choices=data['questionAnswered']
 
 def process_inner(cur):
-    if isinstance(cur,list):
-        return [ process_inner(l) for l in cur ]
-    elif isinstance(cur,dict):
+    if isinstance(cur, list):
+        return [process_inner(l) for l in cur]
+    elif isinstance(cur, dict):
         if "HasRepresentation" in cur:
-            a=cur['HasRepresentation']
-            if isinstance(a,str):
-                cur['HasRepresentation'] = { "Name" : a }
+            a = cur['HasRepresentation']
+            if isinstance(a, str):
+                cur['HasRepresentation'] = {"Name": a}
             return cur
         else:
-            return  { k:process_inner(v) for (k,v) in cur.items()   }
+            return {k: process_inner(v) for (k, v) in cur.items()}
 
     else:
         return cur
 
 
-def process_json(cur):
-    if not args.update:
+def process_json(update, cur):
+    if not update:
         return cur
     if "AnsweredRepr" not in cur:
         return cur
 
     return process_inner(cur)
 
-new_choices = map(process_json, choices)
 
-with (eprime.with_suffix('.choices-eprime.json')).open('w') as f:
-    f.write("[  ")
-    f.write( "\n,  ".join(json.dumps(c)  for c in new_choices) )
-    f.write("\n]")
+def main(eprime, *, update):
+    eprime = Path(eprime)
+    with eprime.open("r") as f:
+        json_lines = [line[1:] for line in f if line[0] == '$']
+
+    data = json.loads(" ".join(json_lines[1:]))
+    choices = data['questionAnswered']
+
+    new_choices = [process_json(update, c) for c in choices]
+
+    with (eprime.with_suffix('.choices-eprime.json')).open('w') as f:
+        f.write("[  ")
+        f.write("\n,  ".join(json.dumps(c) for c in new_choices))
+        f.write("\n]")
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("eprime")
+    parser.add_argument('--update', action='store_true', help='Update old choices')
+    args = parser.parse_args()
+    main(args.eprime, update=args.update)
--- toolchain/reduce_error.py
+++ toolchain/reduce_error.py
@@ -8,77 +8,89 @@ import logging
 import shlex
 import subprocess
 import sys
+import eprime_to_choices
+import datetime
+
 from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("essence", help='essence file')
-parser.add_argument('-o',  dest='output',          help='output base dir ', required=True)
-parser.add_argument('-p',  dest='spec_time',       help='time per run', required=True, type=int)
-parser.add_argument('-t',  dest='total_time',      help='Total real time', type=int)
-parser.add_argument('-bin_dir',  dest='bin_dir',   help='--bin-dir' )
-parser.add_argument('-c',  dest='cores',           help='cores to use, default 1', type=int)
-parser.add_argument('--passing_db',                help='for gen reduce --db-passing-in')
-
+parser.add_argument('-o', dest='output', help='output base dir ', required=True)
+parser.add_argument('-p', dest='spec_time', help='time per run', required=True, type=int)
+parser.add_argument('-t', dest='total_time', help='Total real time', type=int)
+parser.add_argument('-bin_dir', dest='bin_dir', help='--bin-dir')
+parser.add_argument('-c', dest='cores', help='cores to use, default 1', type=int)
+parser.add_argument('--passing_db', help='for gen reduce --db-passing-in')
 
 args = parser.parse_args()
 
 essence = Path(args.essence)
 essence_dir = essence.parent
 
-cmd_str="""
-     gen reduce {essence_dir} -o '{output}' -p {spec_time}  --kind {kind} --status {status} --delete-passing --delete-steps -N  --db-dir '{db_dir}'
+cmd_str = """
+     gen reduce {essence_dir} -o '{output}' -p {spec_time}  --kind {kind} --status {status} --delete-steps -N  --db-dir '{db_dir}'
 """
 
 if args.passing_db:
-    cmd_str+= " --db-passing-in '{}'".format(args.passing_db)
+    cmd_str += " --db-passing-in '{}'".format(args.passing_db)
 
 if args.total_time is not None:
-    cmd_str+= " --total-is-real-time --total-time '{}'".format(args.total_time)
+    cmd_str += " --total-time '{}'".format(args.total_time)
 
 logger.warn("args %s", args)
 
 
-def process(status, kind,refinement, name, vals,refine_times,cmd_str,is_last):
+def process(status, kind, refinement, name, vals, refine_times, cmd_str, is_last):
     if is_last:
-        cmd_str+=" --db-only-passing "
+        cmd_str += " --db-only-passing "
 
     if status == "ErrorUnknown_":
         status = "StatusAny_"
 
-    spec_time=args.spec_time
+    spec_time = args.spec_time
     db_dir = Path(args.output) / "temp_db"
+
+    ts = int(datetime.datetime.now().timestamp())
     if refinement:
         if 'choices_json' in vals['vals']:
-            cs=Path(vals['vals']['choices_json']).name
+            cs = Path(vals['vals']['choices_json']).name
             choices = essence_dir / cs
-
 
             if choices.suffixes == [".choices", ".json"]:
                 new_name = choices.name.replace(".choices.json", ".choices-new.json")
                 if (essence_dir / new_name).exists():
                     choices = essence_dir / new_name
-
-            oxut_dir = Path(args.output) / choices.stem
-
+            out_dir = Path(args.output) / "{}_r-.{}@{}".format(
+                essence_dir.name, choices.stem.replace('.choices-new', '.choices'), ts)
             cmd_str += " --choices {}".format(choices)
-
         else:
-            out_dir = Path(args.output) / "all"
+            out_dir = Path(args.output) / ("all_" + ts)
 
         cpu_used = vals['cpu_time']
-        spec_time = min( max(cpu_used * 2, 20), spec_time)
+        spec_time = min(max(cpu_used * 2, 20), spec_time)
 
     else:
-        cmd_str += " --choices {}".format( (essence_dir / name).with_suffix('.eprime')  )
-        out_dir = Path(args.output) / name
-        cpu_used = refine_times[name] + vals['total_cpu_time']
-        spec_time = min( max(cpu_used * 2, 20), spec_time)
+        eprime = (essence_dir / name).with_suffix('.eprime')
+        eprime_to_choices.main(eprime, update=True)
 
-    spec_time=int(spec_time)
-    cmd_str = cmd_str.format(essence_dir=essence_dir, kind=kind, status=status,
-                       spec_time=spec_time, output=out_dir, db_dir=db_dir)
+        cmd_str += " --choices {}".format(
+            (essence_dir / name).with_suffix('.choices-eprime.json'))
+
+        out_dir = Path(args.output) / "{}_r-.{}.choices@{}".format(essence_dir.name, name,
+                                                                   ts)
+
+        cpu_used = refine_times[name] + vals['total_cpu_time']
+        spec_time = min(max(cpu_used * 2, 20), spec_time)
+
+    spec_time = int(spec_time)
+    cmd_str = cmd_str.format(essence_dir=essence_dir,
+                             kind=kind,
+                             status=status,
+                             spec_time=spec_time,
+                             output=out_dir,
+                             db_dir=db_dir)
 
     if args.bin_dir:
         cmd_str += " --bin-dir {}".format(args.bin_dir)
@@ -86,14 +98,14 @@ def process(status, kind,refinement, name, vals,refine_times,cmd_str,is_last):
     if args.cores:
         cmd_str += " --cores {}".format(args.cores)
 
-
-    cmd_arr=shlex.split(cmd_str)
+    cmd_arr = shlex.split(cmd_str)
     print("running {}".format(cmd_str))
     try:
         subprocess.check_call(cmd_arr)
     except subprocess.CalledProcessError as e:
-        print("error for {} - {} ".format(args.essence, name) )
+        print("error for {} - {} ".format(args.essence, name))
         sys.exit(e.returncode)
+
 
 def lookahead(iterable):
     it = iter(iterable)
@@ -103,33 +115,61 @@ def lookahead(iterable):
         last = val
     yield last, True
 
-if (essence_dir / "solve_eprime.json").exists():
 
+kind = None
+status = None
+
+if (essence_dir / "dir_error.json").exists():
+    with (essence_dir / "dir_error.json").open() as f:
+        data = json.load(f)
+        kind = data['kind']
+        status = data['status']
+
+if (essence_dir / "solve_eprime.json").exists():
     with (essence_dir / "refine_essence.json").open() as f:
-        refine_times = { k:vs['cpu_time']  for (k,vs) in  json.load(f)['data_'].items()   }
+        refine_times = {k: vs['cpu_time'] for (k, vs) in json.load(f)['data_'].items()}
 
     with (essence_dir / "solve_eprime.json").open() as f:
         data = json.load(f)
 
-    for ((name, vals),is_last) in lookahead(data['data_'].items()):
-        kind   = vals['last_kind']
-        status = vals['last_status']
-        process(status, kind, refinement=False, name=name,
-                vals=vals, refine_times=refine_times,cmd_str=cmd_str,is_last=is_last)
-
+    for ((name, vals), is_last) in lookahead(data['data_'].items()):
+        if not kind:
+            kind_ = vals['last_kind']
+        else:
+            kind_ = kind
+        if not status:
+            status_ = vals['last_status']
+        else:
+            status_ = status
+        process(status_, kind_,
+                refinement=False,
+                name=name,
+                vals=vals,
+                refine_times=refine_times,
+                cmd_str=cmd_str,
+                is_last=is_last)
 
 elif (essence_dir / "refine_essence.json").exists():
     with (essence_dir / "refine_essence.json").open() as f:
-        data=json.load(f)
+        data = json.load(f)
 
-
-    for ((name, vals),is_last) in lookahead(data['data_'].items()):
-        kind   = vals['kind_']
-        status = vals['status_']
-        process(status, kind, refinement=True, name=name,
-                vals=vals, refine_times=None, cmd_str=cmd_str, is_last=is_last)
+    for ((name, vals), is_last) in lookahead(data['data_'].items()):
+        if not kind:
+            kind_ = vals['kind_']
+        else:
+            kind_ = kind
+        if not status:
+            status_ = vals['status_']
+        else:
+            status_ = status
+        process(status_, kind_,
+                refinement=True,
+                name=name,
+                vals=vals,
+                refine_times=None,
+                cmd_str=cmd_str,
+                is_last=is_last)
 
 else:
     print("no solve_eprime.json or refine_essence.json file found")
     sys.exit(3)
-
