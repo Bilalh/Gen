9f96c287158030eefd363d75e4a72bdbc5dfcf8b
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-09-17 03:28:36 +0100

    Run a race and collecting the results works

--- prob006-GR/run_race.sh
+++ prob006-GR/run_race.sh
@@ -6,14 +6,15 @@ pushd ..
 export PARAM_GEN_SCRIPTS="$PWD/scripts/"
 popd
 
-export CORES=2
+export NUM_JOBS=3
 export USE_MODE=df
 export OUT_BASE_DIR="$PWD/out"
 
 # export MODELS_TO_USE=
 
+
 now="$(date +%s)"
-param_path="$PWD/./params/1.param"
+param_path="$PWD/./params/6e0c6b668.param"
 time_per_model=60
 working_dir="$PWD"
 
@@ -22,3 +23,14 @@ mkdir out
 
 "$PARAM_GEN_SCRIPTS/wrappers/run.sh" \
 	"$now"  "${param_path}" "${time_per_model}" "${working_dir}"
+
+export USE_DATE="${now}"
+export TOTAL_TIMEOUT="${time_per_model}"
+param_hash="6e0c6b668"
+
+# Need to do this first globally
+# cabal install split
+
+"$PARAM_GEN_SCRIPTS/wrappers/run_gather.sh" \
+	"${param_hash}" "${working_dir}"
+
