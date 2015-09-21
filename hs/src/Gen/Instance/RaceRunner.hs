b81aa1423624f4a21dc1c77698ab9ea7453fc261
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-09-21 03:55:09 +0100

    param essence

--- hs/src/Gen/Instance/RaceRunner.hs
+++ hs/src/Gen/Instance/RaceRunner.hs
@@ -17,13 +17,22 @@ import qualified Data.Set as S
 createParamEssence ::  (Sampling a, MonadState (Method a) m, MonadIO m) => m ()
 createParamEssence = do
   (Method MCommon{mEssencePath,mVarInfo, mOutputDir} _) <- get
-  let specFp = (mOutputDir </> "essence_param_find.essence")
+  let specFp   = (mOutputDir </> "essence_param_find.essence")
+  let eprimeFp = (mOutputDir </> "essence_param_find.eprime")
+
   liftIO $ doesFileExist specFp >>= \case
     True  -> return ()
     False -> do
       model     <- liftIO $ readModelFromFile mEssencePath
       paramSpec <- liftIO $ createParamSpecification model mVarInfo
       writeModel PlainEssence (Just "essence_param_find.essence") paramSpec
+
+  liftIO $ doesFileExist eprimeFp >>= \case
+    True  -> return ()
+    False -> do
+      conjureCompact specFp eprimeFp >>= \case
+        True  -> return ()
+        False -> userErr1 "Failed to refine the param specification, namely essence_param_find.essence "
 
 
 sampleParamFromMinion :: (Sampling a, MonadState (Method a) m, MonadIO m) => Point -> m ()
@@ -68,12 +77,16 @@ conjureCompact inn out = do
         renameFile (tmp </> "model000001.eprime") out
         return True
 
-ex2 = do
+
+_ex1, _ex2 :: IO ()
+
+
+_ex2 = do
   res <- conjureCompact "/Users/bilalh/CS/instancegen-models/_new/prob013-PPP/prob013-PPP.essence"
                         "/Users/bilalh/CS/instancegen-models/_new/prob013-PPP/a.eprime"
   print res
 
-ex1 = do
+_ex1 = do
   i :: VarInfo <- readFromJSON "/Users/bilalh/CS/instancegen-models/_new/prob013-PPP/info.json"
   m <- readModelFromFile "/Users/bilalh/CS/instancegen-models/_new/prob013-PPP/prob013-PPP.essence"
   param <- createParamSpecification m i
