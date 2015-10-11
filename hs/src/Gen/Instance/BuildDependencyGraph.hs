3dc4ca0d0284dbe088b2c0936932bd1b02658385
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-10-11 22:09:09 +0100

    mv

---
+++ hs/src/Gen/Instance/BuildDependencyGraph.hs
@@ -0,0 +1,5 @@
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
+module Gen.Instance.Nsample(Nsample(..),Sampling(..)) where
+
+import Gen.Imports
+import Gen.Instance.Point         (pointHash,Distance(..))
