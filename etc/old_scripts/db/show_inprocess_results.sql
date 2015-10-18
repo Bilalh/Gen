97cb6592262b7668919b2a33b90941bcc2b2d86e
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-10-18 17:56:58 +0100

    move stuff

--- etc/old_scripts/db/show_inprocess_results.sql
+++
@@ -1,9 +0,0 @@
-.mode column
-.width 9 7 8 11 11 12 7 7 7
-.headers on
-Select paramHash, quality, ordering,
-	eprimesLeft, Satisfiable, MaxSolutions,
-	minTime, maxTime, avgTime
-From ParamsData
-Limit 10
-;
