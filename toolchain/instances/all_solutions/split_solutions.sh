0f9bbf3a606a1e5af88fe97c9c1961b8c0646141
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-10-20 02:01:09 +0100

    split solutions

---
+++ toolchain/instances/all_solutions/split_solutions.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o nounset
+set -e
+
+echo "NUM_JOBS is ${NUM_JOBS}"
+
+dir="$1"
+cd "$dir"
+
+function process(){
+    [ "$1" = "" ] && return 0
+    gsplit -d -a10 -l 1000000 "$1" "$1."
+    tar -c "$1" | pigz -c -p"${NUM_JOBS}" > "${1}".tar.gz
+    rm "$1"
+}
+export -f process
+
+parallel -j1 "process" ::: "$(find . -name '*minion-solution')"
+
+echo "Finished"
