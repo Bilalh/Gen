c46d12c00c50336a711513c344ef9173bcd43c1d
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-11-14 23:16:52 +0000

    pretty

--- hs/src/Gen/Instance/SamplingError.hs
+++ hs/src/Gen/Instance/SamplingError.hs
@@ -22,10 +22,14 @@ data SamplingErr = ErrRejectedPoint Doc
 instance  Pretty SamplingErr  where
   pretty (ErrRejectedPoint d)         = hang "ErrRejectedPoint" 4 d
   pretty (ErrDuplicatedPoint d)       = hang "ErrDuplicatedPoint" 4 d
+  pretty (ErrCompact d)               = hang "ErrCompact" 4 d
+  pretty (ErrDB d)                    = hang "ErrDB" 4 d
   pretty (ErrNoValuesLeft d)          = hang "ErrNoValuesLeft" 4 d
   pretty (ErrFailedToGenerateParam d) = hang "ErrFailedToGenerateParam" 4 d
+  pretty (ErrFailedRunSolve d)        = hang "ErrFailedRunSolve" 4 d
   pretty (ErrRace d)                  = hang "ErrRace" 4 d
-  pretty (ErrRaceSrInit d)            = hang "ErrRace" 4 d
+  pretty (ErrRaceSrInit d)            = hang "ErrRaceSrInit" 4 d
   pretty (ErrGather d)                = hang "ErrGather" 4 d
   pretty (ErrFail d)                  = hang "ErrFail" 4 d
-  pretty x                            = hang "noPretty" 4 (pretty $ groom x)
+  pretty (ErrAllSolutions d)          = hang "ErrAllSolutions" 4 d
+  -- pretty x                            = hang "noPretty" 4 (pretty $ groom x)
