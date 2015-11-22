a41cb3d95cdde96b41739ec8fe1448e9dbd5e35a
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-11-22 16:23:30 +0000

    settings

--- hs/src/Gen/IO/SmacProcess.hs
+++ hs/src/Gen/IO/SmacProcess.hs
@@ -18,7 +18,9 @@ import Gen.IO.Formats                   (readFromJSON, readFromJSONMay)
 import System.Directory                 (getHomeDirectory)
 import System.Exit                      (ExitCode (ExitSuccess))
 import System.FilePath                  (takeDirectory)
+import  Gen.Instance.Results.Results
 
+import qualified Data.Vector              as V
 import qualified Data.Map                 as M
 import qualified Data.Set                 as Set
 import qualified Data.Text                as T
@@ -28,4 +30,8 @@ import qualified Gen.Instance.Results.SettingsIn  as IN
 smacProcess s_output_directory s_eprime s_instance_specific
   s_cutoff_time s_cutoff_length s_seed s_param_arr = do
 
+  vs <- liftIO $ V.toList <$> decodeCSV (s_output_directory </> "settings.csv")
+  let x@IN.CSV_IN{..} = headNote "setting.csv should have one row" vs
+
   liftIO $ print "smacProcess"
+  liftIO $ groomPrint x
