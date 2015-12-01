54477f3517cf7d77c79a7288d891f8f7776edc6b
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-12-01 01:06:29 +0000

    init state

--- hs/src/Gen/IO/FindCompact.hs
+++ hs/src/Gen/IO/FindCompact.hs
@@ -1,12 +1,42 @@
-{-# LANGUAGE TupleSections, DeriveGeneric #-}
-module Gen.IO.FindCompact(findCompact) where
+{-# LANGUAGE DeriveGeneric, TupleSections #-}
+module Gen.IO.FindCompact(findCompact, lookupCompact) where
 
 import Crypto.Hash
-import Gen.Imports hiding (hash)
+import Gen.Imports     hiding (hash)
+import System.FilePath (replaceFileName, takeBaseName)
 
 import qualified Data.ByteString.Char8 as C
 
 type Dir = FilePath
+
+-- the code in gen main should use this
+lookupCompact :: MonadIO m => Dir -> FilePath -> m (Maybe FilePath)
+lookupCompact models_path essence_path  = do
+
+  let compactPaths = [ (models_path ++ "-compact")
+                     , replaceFileName essence_path (takeBaseName essence_path ++ "_" ++ "df-compact")
+                     ]
+  cExists <- liftIO$ filterM doesDirectoryExist compactPaths
+
+  compactFirst <- case cExists of
+    [] -> do
+      liftIO $ print . pretty $  "no compact found, at"  <+> vcat (map pretty compactPaths)
+      return Nothing
+    (path:_)  -> do
+      liftIO $  print . pretty $ "checking for compact in: " <+> pretty path
+      cs <- liftIO $ getAllFilesWithSuffix ".eprime" path
+      catMaybes <$> mapM ((flip findCompact) models_path) cs  >>= \case
+        []   -> return Nothing
+        [x]  -> do
+          liftIO $  print . pretty $ "compact_twin is" <+> pretty x
+          return $ Just $ (takeBaseName x)
+        (x:_) -> do
+          liftIO $  print . pretty $ "Picking first compact_twin" <+> pretty x
+          return $ Just $ (takeBaseName x)
+
+  return compactFirst
+
+
 findCompact :: MonadIO m => FilePath -> Dir -> m (Maybe FilePath)
 findCompact compactFP dir = do
   to_check <- liftIO $ getAllFilesWithSuffix ".eprime" dir
--- hs/src/Gen/IO/SmacProcess.hs
+++ hs/src/Gen/IO/SmacProcess.hs
@@ -18,18 +18,25 @@ import Database.SQLite.Simple.FromRow   ()
 import Gen.Helpers.Str
 import Gen.Imports                      hiding (group)
 import Gen.Instance.Data
-import Gen.Instance.Point
-import Gen.Instance.Results.Results
 import Gen.Instance.Method
+import Gen.Instance.Point
+import Gen.Instance.RaceRunner          (initDB)
+import Gen.Instance.Results.Results
 import Gen.Instance.UI
+import Gen.Instance.UI
+import Gen.Instance.Undirected
+import Gen.IO.FindCompact
 import Gen.IO.Formats                   (readFromJSON, readFromJSONMay)
 import System.CPUTime                   (getCPUTime)
 import System.Directory                 (getHomeDirectory)
+import System.Directory                 (makeAbsolute)
+import System.Environment               (lookupEnv)
 import System.Exit                      (ExitCode (ExitSuccess))
 import System.FilePath                  (takeDirectory)
+import System.FilePath.Posix            (replaceFileName, takeBaseName)
+import System.IO                        (hPutStrLn, stderr)
+import System.Random                    (mkStdGen, setStdGen)
 import Text.Printf
-import Gen.Instance.Undirected
-import System.IO(hPutStrLn,stderr)
 
 import qualified Data.Map                        as M
 import qualified Data.Set                        as Set
@@ -40,6 +47,7 @@ import qualified Gen.Instance.Results.SettingsIn as IN
 
 smacProcess s_output_directory s_eprime s_instance_specific
   s_cutoff_time s_cutoff_length s_seed s_param_arr = do
+  liftIO $ setStdGen (mkStdGen s_seed)
   startOurCPU <- liftIO $  getCPUTime
 
   vs <- liftIO $ V.toList <$> decodeCSV (s_output_directory </> "settings.csv")
@@ -62,17 +70,48 @@ smacProcess s_output_directory s_eprime s_instance_specific
 -- | Load the state from disk if it exists otherwise init it.
 s_loadState :: MonadIO m => IN.CSV_IN  -> m (Method Undirected)
 s_loadState dat = liftIO $ doesFileExist "state.json" >>= \case
-  False -> return $ s_initState dat
+  False -> s_initState dat
   True  -> readFromJSON "state.json"
 
-s_initState :: IN.CSV_IN -> Method Undirected
-s_initState = $notDone
+s_initState :: MonadIO m => IN.CSV_IN -> m (Method Undirected)
+s_initState IN.CSV_IN{..} = do
+  essenceA <- liftIO $ makeAbsolute essence
+  let info_path   = replaceFileName essenceA "info.json"
+      models_path = replaceFileName essenceA (takeBaseName essenceA ++ "_" ++ mode)
+
+  compactFirst <- lookupCompact models_path essenceA
+  cores        <- liftIO $ fromJustNote "CORES must be set" <$> lookupEnv "CORES"
+
+  i <- liftIO $ readFromJSON info_path
+  p <- ignoreLogs $ makeProvider essence i
+
+  let common          = MCommon{
+        mEssencePath    = essence
+      , mOutputDir      = "."
+      , mModelTimeout   = per_model_time_given
+      , mVarInfo        = i
+      , mPreGenerate    = Nothing
+      , mIterations     = iterations
+      , mMode           = mode
+      , mModelsDir      = models_path
+      , mGivensProvider = p
+      , mPoints         = []
+      , mCores          = 4
+      , mCompactName    = compactFirst
+      , mSubCpu         = 0
+      , mPointsGiven    = Nothing
+      , mParamGenTime   = 300
+      }
+
+  return $ Method common Undirected
 
 
 s_runMethod :: (Sampling a, MonadState (Method a) m, MonadIO m, MonadLog m, ToJSON a)
-            => m ()
-s_runMethod = do
-  $notDone
+            => Bool -> Method Undirected ->  m ()
+s_runMethod initValues state = do
+  void $ flip execStateT state $ do
+    when initValues $ initDB >> doSaveEprimes True
+    handleWDEG >> run
 
 -- | This needs to be the last line
 outputResult :: MonadIO m => String -> Double -> Int -> Int -> Int -> m ()
@@ -80,5 +119,6 @@ outputResult result_kind runtime runlength quality seed = do
   liftIO $ printf "Final Result for ParamILS: %s, %f, %d, %d, %d\n"
           result_kind runtime runlength quality seed
 
+-- | Allows us to see the output in the logs
 out :: MonadIO m => String -> m ()
 out s = liftIO $ hPutStrLn stderr $ unlines [ '»' : ' ' : xs | xs <- lines s ]
