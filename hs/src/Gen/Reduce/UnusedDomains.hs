cb3577f72181d68f80131877ca57f9109f301fc3
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-12-06 04:26:41 +0000

    Check for domain references inside domains as well

--- hs/src/Gen/Reduce/UnusedDomains.hs
+++ hs/src/Gen/Reduce/UnusedDomains.hs
@@ -6,7 +6,14 @@ import qualified Data.Map as M
 
 unusedDomains :: Spec -> [Text]
 unusedDomains (Spec ds es obj)=
-  let used = S.fromList $ [y |  x <- (es ++ maybeToList (fmap snd obj) )
-                             ,  EVar (Var y _) <- universe x ]
+  let inExprs = S.fromList $ [y |  x <- (maybeToList (fmap snd obj) ++ es)
+                                ,  EVar (Var y _) <- universe x ]
+
+      inDoms     = S.fromList $ concatMap (checkDom . domOfGF . snd) (M.elems ds)
+      checkDom :: Domain () Expr -> [Text]
+      checkDom x = [ y | EVar (Var y _) <- universe (EDom x) ]
+
+      used = S.union inExprs inDoms
+
       unused = M.filterWithKey (\k _ -> not $ k `S.member` used  ) ds
   in M.keys unused
