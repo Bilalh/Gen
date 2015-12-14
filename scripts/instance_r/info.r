26bc594f18315796e355e55a0d356178246405ce
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-12-14 23:34:34 +0000

    r

--- scripts/instance_r/info.r
+++ scripts/instance_r/info.r
@@ -7,20 +7,22 @@ library(plyr)
 base <- path.expand("~/Desktop/Results/sampling_no_large/")
 all <- read.csv(file.path(base, "all.csv"))
 
-info=ddply(all, c("essenceClass", "kind", "heuristic", "mode","group", "refineGroup" ), summarise,
+info=ddply(all, c("essenceClass", "kind", "heuristic", "mode","num_models", "per_model_time_given", "group", "refineGroup" ), summarise,
            runs=length(run_no),
            highestOrderingMean=mean(highestOrderingNeeded),
            highestOrderingNeeded=toString(highestOrderingNeeded),
            compactWon=toString(compactWon),
            numFractures=toString(numFractures),
            fracturesSize=toString(fracturesSize),
-           # rIterationsDone=mean(rIterationsDone),
            rIterationsDoneIncludingFailed=toString(rIterationsDoneIncludingFailed),
            hostType=toString(hostType),
+           seq=toString(seq),
            rCPUTime=mean(rCPUTime),
            rRealTime=mean(rRealTime),
            fCPUTime=mean(rCPUTime)/3600,
-           fRealTime=mean(rRealTime)/3600
+           fRealTime=mean(rRealTime)/3600,
+           compact=toString(compact),
+           fractures=toString(fractures)
            )
 
 
@@ -34,7 +36,13 @@ selected.ppp  <- all[ all$essenceClass=="prob013-PPP"       &  all$kindClass=="u
 selected.wh   <- all[ all$essenceClass=="prob034-warehouse" &  all$kindClass=="undirected" & all$mode=="sample-64" , ]
 selected.bacp <- all[ all$essenceClass=="prob030-BACP"      &  all$kindClass=="undirected" & all$mode=="sample-64" , ]
 selected.efpa <- all[ all$essenceClass=="prob055-efpa"      &  all$kindClass=="undirected" & all$mode=="sample-64" , ]
-selected.sgp <- all[ all$essenceClass=="prob010-SGP"        &  all$kindClass=="undirected" , ]
+selected.sgp  <- all[ all$essenceClass=="prob010-SGP"       &  all$kindClass=="undirected" , ]
+
+info.ppp  <- info[ info$essenceClass=="prob013-PPP" , ]
+info.wh   <- info[ info$essenceClass=="prob034-warehouse" , ]
+info.sgp  <- info[ info$essenceClass=="prob010-SGP" , ]
+
+info2 <- info[ info$refineGroup!="2015-11-06_symlink" , ]
 
 # View(selected)
 
