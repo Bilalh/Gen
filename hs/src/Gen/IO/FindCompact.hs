00a76c8cb410221650d10700864857b003b73bb8
Bilal Syed Hussain <bilalshussain@gmail.com>
2015-12-17 02:52:23 +0000

    better compact recording

--- hs/src/Gen/IO/FindCompact.hs
+++ hs/src/Gen/IO/FindCompact.hs
@@ -4,8 +4,12 @@ module Gen.IO.FindCompact(findCompact, lookupCompact) where
 import Crypto.Hash
 import Gen.Imports     hiding (hash)
 import System.FilePath (replaceFileName, takeBaseName)
+import Conjure.UI.NormaliseQuantified(normaliseQuantifiedVariables)
+import Conjure.UI.IO(readModel)
+import Conjure.Language.Pretty
 
 import qualified Data.ByteString.Char8 as C
+import qualified Data.Text as T
 
 type Dir = FilePath
 
@@ -40,17 +44,21 @@ lookupCompact models_path essence_path  = do
 findCompact :: MonadIO m => FilePath -> Dir -> m (Maybe FilePath)
 findCompact compactFP dir = do
   to_check <- liftIO $ getAllFilesWithSuffix ".eprime" dir
-  compact_hash <- hashFileStrict compactFP
-  hashes <- mapM hashFileStrict to_check
+  compact_hash <- hashModel compactFP
+  hashes <- mapM hashModel to_check
   case [  path | (h,path) <- zip hashes to_check, h == compact_hash ] of
     []  -> return Nothing
     [x] -> return (Just x)
     _   -> error "Multiple Models match compact"
 
-hashFileStrict :: MonadIO m => FilePath -> m (Digest MD5)
-hashFileStrict fp = liftIO $ do
-  content <- C.readFile fp
-  let slns = C.concat $ [ x | x <- C.lines content
-                        -- eprime choices
-                        , not $ "$"   `C.isPrefixOf` x]
-  return $ hash slns
+hashModel :: MonadIO m => FilePath -> m (Digest MD5)
+hashModel fp = do
+  pair <- liftIO $ pairWithContents fp
+  m <- liftIO $ readModel stripComments pair
+  let normalised = normaliseQuantifiedVariables m
+  let encoded = C.pack $ renderNormal normalised
+  return $ hash encoded
+
+
+stripComments :: Text -> Text
+stripComments = T.unlines . map (T.takeWhile (/= '$')) . T.lines
