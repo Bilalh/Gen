dba44d3537c5b83bba01bf839f8ce6f7afc359df
Bilal Syed Hussain <bilalshussain@gmail.com>
2016-01-17 19:03:48 +0000

    pcs

---
+++ scripts/pcs/rehearsal_pcs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import argparse
+
+parser = argparse.ArgumentParser(prog='Rehearsal_pcs')
+# args = parser.parse_args()
+args = argparse.Namespace(n_pieces=4, n_players=4)
+
+for e in ["n_pieces", "n_players"]:
+  print("{:11} [1,{}] [{}]i".format(e, getattr(args, e), getattr(args, e) // 2))
+
+print()
+print()
+
+for i in range(1, args.n_players * args.n_pieces + 1):
+  print("plays_in%rel%{:03d}   [0,1] [0]i".format(i))
+  print("plays_in%rel%{:03d}%{:01d} [1,{}] [{}]i".format(i, 1, args.n_players,
+                                                         args.n_players // 2))
+  print("plays_in%rel%{:03d}%{:01d} [1,{}] [{}]i".format(i, 2, args.n_pieces,
+                                                         args.n_pieces // 2))
+  print()
